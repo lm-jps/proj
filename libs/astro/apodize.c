7dbe1f5950dc202564b3111ece45b9bbbffe7d74
arta <arta>
2008-04-23 21:41:41 +0000

    Re-port apodize function - there may have been some rounding errors introduced in the last port

--- libs/astro/apodize.c
+++ libs/astro/apodize.c
@@ -54,119 +54,92 @@ int apodize(float *data,		/* input/output data array */
 	    double apy)		/* divide the y position by this before applying 
 				   apodization */
 {
-   int row, col;
-   float *dp;
-   double sinBbase;
-   double sinB0, cosB0;
-   double sinB, cosB;
-   double L;
-   double apouter,apinx,apoutx,ss,cc,sc,cs;
-   double x, y, r, cosrho, apx2, apy2;
-   double *savecosL, *savesinL;
+int row, col, i;
+double weight;
+float *dp;
+double sinBbase;
+double sinB0, cosB0;
+double sinB, cosB;
+double L;
+double apouter,apinx,apoutx,ss,cc,sc,cs;
+double x, y, r, cosrho, apod, apx2, apy2;
+double *savecosL, *savesinL;
 
-   if (apodlevel == 0) 
-   {
-      return kLIBASTRO_Success;
-   }
+if (apodlevel == 0) return kLIBASTRO_Success; /* do nothing */
 
-   if (apodlevel > 2) 
-   {
-      return kLIBASTRO_InconsistentConstraints;
-   }
+if (apodlevel > 2) return kLIBASTRO_InconsistentConstraints;
 
-   if (apodlevel == 2) 
-   {
-      b0 = 0.0; /* pretend that B0=0.0 */
-   }
+if (apodlevel == 2) b0=0.0; /* pretend that b0=0.0 */
 
-   apouter = apinner + apwidth;
+apouter=apinner+apwidth;
 
-   sinBbase = 0.5 - rows / 2;
-   sinB0 = sin(b0);
-   cosB0 = cos(b0);
-   apinx = sqrt(1. - apinner * apinner);
-   apoutx = sqrt(1. -apouter * apouter);
-   apx2 = 1.0 / apx / apx;
-   apy2 = 1.0 / apy / apy;
+sinBbase=0.5-rows/2;
+sinB0=sin(b0);
+cosB0=cos(b0);
+apinx=sqrt(1.-apinner*apinner);
+apoutx=sqrt(1.-apouter*apouter);
+apx2=1.0/apx/apx;
+apy2=1.0/apy/apy;
 
-   /* Save some time by setting up arrays first */
-   savecosL = (double *)(malloc(cols * sizeof(double)));
-   savesinL = (double *)(malloc(cols * sizeof(double)));
-   for (col = 0; col < cols; col++) 
-   {
-      L = Ldelta * col + Lmin;
-      savecosL[col] = cos(L);
-      savesinL[col] = sin(L);
-   }
+/* Save some time by setting up arrays first */
+savecosL=(double *)(malloc(cols*sizeof(double)));
+savesinL=(double *)(malloc(cols*sizeof(double)));
+for (col=0; col<cols; col++) {
+  L=Ldelta*col+Lmin;
+  savecosL[col]=cos(L);
+  savesinL[col]=sin(L);
+}
 
-   for (row = 0; row < rows; row++) 
-   {
-      sinB = sinBdelta * (sinBbase + row);
-      cosB = sqrt(1.0 - sinB * sinB);
-      ss = sinB0 * sinB;
-      cc = cosB0 * cosB;
-      sc = sinB0 * cosB;
-      cs = cosB0 * sinB;
-      dp = data + row * cols;
-      if (apel == 0) 
-      {
-	 for (col = 0; col < cols; col++) 
-	 {
-	    /*    Old code
-		  L=Ldelta*col+Lmin;
-		  cosrho=sinB0*sinB+cosB0*cosB*cos(L);
-		  r=sqrt(1.0-cosrho*cosrho);
-		  if (r < apinner)
-		  apod=1;
-		  else if (r < apouter)
-		  apod=0.5+0.5*cos(PI*(r-apinner)/apwidth);
-		  else
-		  apod=0.0;
-		  dp[col]*=apod;
-	    */
-	    /*   New code, much faster.
-		 All points outside apouter are turned into 0.0, no NaNs */
-	    cosrho = ss + cc * savecosL[col];
-	    if (cosrho > apinx) 
-	    {
-	       /* XXX why is this here? */
-	    }
-	    else if (cosrho > apoutx) 
-	    {
-	       dp[col] *= (float )
-		 (0.5 + 0.5 * cos(PI * (sqrt(1.0 - cosrho * cosrho) - apinner) / apwidth));
-	    }
-	    else
-	    {
-	       dp[col] = 0.0;
-	    }
-	 }
+for (row = 0; row < rows; row++) {
+  sinB=sinBdelta*(sinBbase + row);
+  cosB=sqrt(1.0-sinB*sinB);
+  ss=sinB0*sinB;
+  cc=cosB0*cosB;
+  sc=sinB0*cosB;
+  cs=cosB0*sinB;
+  dp=data+row*cols;
+  if (apel == 0) {
+    for (col=0; col<cols; col++) {
+/*    Old code
+      L=Ldelta*col+Lmin;
+      cosrho=sinB0*sinB+cosB0*cosB*cos(L);
+      r=sqrt(1.0-cosrho*cosrho);
+      if (r < apinner)
+        apod=1;
+      else if (r < apouter)
+        apod=0.5+0.5*cos(PI*(r-apinner)/apwidth);
+      else
+        apod=0.0;
+      dp[col]*=apod;
+*/
+/*   New code, much faster.
+     All points outside apouter are turned into 0.0, no NaNs */
+      cosrho=ss+cc*savecosL[col];
+      if (cosrho > apinx) {}
+      else if (cosrho > apoutx) {
+        dp[col]*=(0.5+0.5*cos(PI*(sqrt(1.0-cosrho*cosrho)-apinner)/apwidth));
       }
-      else 
-      {
-	 /*  Do elliptical apodization, some old, some new code */
-	 for (col = 0; col < cols; col++) 
-	 {
-	    x = cosB * savesinL[col];
-	    y = cs - sc * savecosL[col];
-	    r = sqrt(apx2 * x * x + apy2 * y * y);
-	    if (r < apinner)
-	    {
-	       /* XXX why is this here? */
-	    }
-	    else if (r < apouter)
-	    {
-	       dp[col] *= (float )(0.5 + 0.5 * cos(PI * (r - apinner) / apwidth));
-	    }
-	    else
-	    {
-	       dp[col] = 0.0;
-	    }
-	 }
-      }
-   }
+      else
+        dp[col]=0.0;
+    }
+  }
+  else {
+/*  Do elliptical apodization, some old, some new code */
+    for (col=0; col<cols; col++) {
+      x=cosB*savesinL[col];
+      y=cs-sc*savecosL[col];
+      r=sqrt(apx2*x*x+apy2*y*y);
+      if (r < apinner) {}
+      else if (r < apouter)
+        dp[col]*=0.5+0.5*cos(PI*(r-apinner)/apwidth);
+      else
+        dp[col]=0.0;
+    }
+  }
+}
 
-   free(savesinL);
-   free(savecosL);
-   return kLIBASTRO_Success;
+free(savesinL);
+free(savecosL);
+
+return kLIBASTRO_Success;
 }
