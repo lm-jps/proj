11c3888bcbc19e7cc98d2f74ffdcbb9d8004aaa6
arta <arta>
2008-05-19 00:02:29 +0000

    Add drms_keyword_snprintfval() - clients were using drms_sprintfval_format() expecting it to work the same way for all keyword types, but it treats time different than the rest.  So use drms_keyword_snprintfval()

--- export/apps/jsoc_info.c
+++ export/apps/jsoc_info.c
@@ -308,14 +308,8 @@ void drms_sprint_rec_query(char *text, DRMS_Record_t *rec)
       char val[1000];
       pkey = external_pkeys[iprime];
       rec_key = drms_keyword_lookup (rec, pkey, 1);
-      if (rec_key->info->type == DRMS_TYPE_TIME)
-      {
-	 drms_sprintfval_format(val, rec_key->info->type, &(rec_key->value), rec_key->info->unit, 0);
-      }
-      else
-      {
-	 drms_sprintfval_format(val, rec_key->info->type, &(rec_key->value), rec_key->info->format, 0);
-      }
+      drms_keyword_snprintfval(rec_key, val, sizeof(val));
+
       strcat(text, "[");
       strcat(text, val);
       strcat(text, "]");
@@ -818,16 +812,8 @@ int DoIt(void)
         else
 	  {
           rec_key_ikey = drms_keyword_lookup (rec, keys[ikey], 1); 
-	  if (rec_key_ikey->info->type == DRMS_TYPE_TIME)
-	  {
-	     drms_sprintfval_format(rawval, rec_key_ikey->info->type, &(rec_key_ikey->value),
-				    rec_key_ikey->info->unit, 0);
-	  }
-	  else
-	  {
-	     drms_sprintfval_format(rawval, rec_key_ikey->info->type, &(rec_key_ikey->value),
-				 rec_key_ikey->info->format, 0);
-	  }
+	  drms_keyword_snprintfval(rec_key_ikey, rawval, sizeof(rawval));
+	  
           switch (rec_key_ikey->info->type)
             {
             case DRMS_TYPE_STRING:
