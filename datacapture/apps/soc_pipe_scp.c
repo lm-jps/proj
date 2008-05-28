31ae5be427621bc3b7d1cd427a58df1b24429146
production <production>
2008-05-28 20:36:25 +0000

    use /usr/bin/scp

--- datacapture/apps/soc_pipe_scp.c
+++ datacapture/apps/soc_pipe_scp.c
@@ -220,7 +220,7 @@ int main(int argc, char *argv[])
     while((dp=readdir(dfd)) != NULL) {
       /* First copy .tlm files. */
       if(cptr=strstr(dp->d_name, ".tlm")) {
-	sprintf(cmd, "scp %s/%s %s:%s 1> /dev/null 2>&1",
+	sprintf(cmd, "/usr/bin/scp %s/%s %s:%s 1> /dev/null 2>&1",
 			sourcedir, dp->d_name, hostname, targetdir);
         printk("%s\n", cmd);
         if(system(cmd)) {
@@ -243,7 +243,7 @@ int main(int argc, char *argv[])
             continue;
           }
         }
-        sprintf(cmd, "scp %s/%s %s:%s 1> /dev/null 2>&1",
+        sprintf(cmd, "/usr/bin/scp %s/%s %s:%s 1> /dev/null 2>&1",
                         sourcedir, dp->d_name, hostname, targetdir);
         printk("%s\n", cmd);
         if(system(cmd)) {
@@ -260,7 +260,7 @@ int main(int argc, char *argv[])
         if(!strcmp(dp->d_name, ".") || !strcmp(dp->d_name, "..") 
 		|| strstr(dp->d_name, ".qac")) //.qac done above with .tlm
           continue;
-        sprintf(cmd, "scp %s/%s %s:%s 1> /dev/null 2>&1",
+        sprintf(cmd, "/usr/bin/scp %s/%s %s:%s 1> /dev/null 2>&1",
 			sourcedir, dp->d_name, hostname, targetdir);
         printk("%s\n", cmd);
         if(system(cmd)) {
