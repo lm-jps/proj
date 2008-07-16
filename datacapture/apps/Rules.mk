bd9af54bf61b03d5c5fe45bcc9e22dfe04c1ee47
arta <arta>
2008-07-16 00:08:19 +0000

    Fix for ingest_tlm not building on gcc - force it to build with icc, despite request to build with gcc.  But this will be built at SU only, and SU has icc

--- datacapture/apps/Rules.mk
+++ datacapture/apps/Rules.mk
@@ -4,8 +4,12 @@ dirstack_$(sp)	:= $(d)
 d		:= $(dir)
 
 # Local variables
+
+# Force icc compilation since this doesn't work for gcc because libhmicomp_egse was built with icc.
+LOCALCC		= $(ICC_COMP)
+LOCALLN		= $(ICC_LINK)
+
 EXE_$(d)	:= $(addprefix $(d)/, ingest_tlm soc_pipe_scp)
-CEXE		:= $(CEXE) $(EXE_$(d))
 CEXESUMS	:= $(CEXESUMS) $(EXE_$(d))
 
 OBJ_$(d)	:= $(EXE_$(d):%=%.o) 
@@ -23,7 +27,13 @@ S_$(d)		:= $(notdir $(EXE_$(d)))
 # Local rules
 $(OBJ_$(d)):	$(SRCDIR)/$(d)/Rules.mk
 $(OBJ_$(d)):	CF_TGT := $(CF_TGT) -DCDIR="\"$(SRCDIR)/$(d)\""
+$(OBJ_$(d)):	%.o:	%.c
+		$(LOCALCC)
+
 $(EXE_$(d)):	LL_TGT :=  -L/home/production/cvs/jsoc/lib/saved/$(JSOC_MACHINE) -lhmicomp_egse -lecpg -lpq
+$(EXE_$(d)):	%:	%.o $(EXELIBS)
+		$(LOCALLN)
+		$(SLBIN)
 
 # Shortcuts
 .PHONY:	$(S_$(d))
