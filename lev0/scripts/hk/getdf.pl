867b3036a1b87bf328cff0516ad47ec80437efe9
carl <carl>
2008-08-06 18:11:56 +0000

    getdf.pl: Updated start and end dates to avoid getting beginning of next day's dayfile for HSB dayfiles. Added count of files loaded in log. Added comments on limitations, description, etc.

--- lev0/scripts/hk/getdf.pl
+++ lev0/scripts/hk/getdf.pl
@@ -1,19 +1,23 @@
 #!/usr/bin/perl
 ##############################################################################
 # Name:        getdf.pl  - get day files, ingest into dayfile data series    #
-#                          then remove file                                  #
+#                          then remove day file                              #
 # Description: Get dayfiles from drop directory for hsb, egsefm(in future)   #
-#              Call ingest_dayfile.pl and valid loaded dayfile . If so       #
+#              Call ingest_dayfile.pl and validate loaded dayfiles. If so    #
 #              remove dayfile from file system.                              #
 # Execution:   getdf.pl hsb                                                  #
-# Limitation:   The move-and-ingest-dayfiles process works for dayfiles from #
-#               the hsb dayfiles only. This script is not used               #
-#               currently to move and  call ingest_dayfiles.pl for           #
-#               LMSAL dayfiles.                                              #
+#              getdf.pl egsefm(in future)                                    #
+# Limitation:  The get dayfiles process works for dayfiles from              #
+#              the hsb dayfiles only. This script is not used                #
+#              currently to move and  call ingest_dayfiles.pl for            #
+#              LMSAL dayfiles. This script does not check if dayfile is in   #
+#              data series before writing to data series, therefore can      #
+#              overwrite.  Script not used for moc prod server dayfiles(see  #
+#              movedf.pl script)                                             #
 ##############################################################################
 # set Environment Variables
 
-# drop off directory for HSB dayfiles
+# drop off directory for location of HSB dayfiles,etc
 $doff_dir=$ENV{'DF_DROPOFF_HSB_FILES'}="/tmp21/production/lev0/hk_hsb_dayfile";
 
 # debug flag
@@ -30,7 +34,7 @@ $ENV{'PATH'}="/usr/local/bin:/bin:/usr/bin:.:$script_dir:$ENV{'DF_DRMS_EXECUTABL
 # check arguments
 &check_agruments();
 
-# set log file based on sdo, moc, or egsefm
+# set log file based on source of dayfiles
 if ($src eq "hsb")
 {
   $logfile="$hm/cvs/JSOC/proj/lev0/scripts/hk/log-df-hsb";
@@ -44,19 +48,32 @@ elsif ($src eq "egsefm")
   $logfile="$hm/cvs/JSOC/proj/lev0/scripts/hk/log-df-egsefm";
 }
 
-# open log file
+# open log file and append
 open(LF,">>$logfile") || die "Can't Open $logfile: $!\n";
 print LF `date`;
-print LF "--->starting script getdf.pl\n";
-print LF "--->processing day files at directory:$doff_dir\n";
-close LF;
+print LF "--->Starting script getdf.pl\n";
+print LF "--->Processing day files at directory:$doff_dir\n";
 
 #ingest all dayfiles files
 # for production
 if  ($src eq "hsb")
 {
-  $log=`/usr/bin/perl  $script_dir/ingest_dayfile.pl  apidlist=$script_dir/df_apid_list_day_file_hsb start=20080801 end=20080830 dsnlist=$script_dir/df_apid_ds_list_for_hsb src=hsb`;
-  print LF "--->Processed day files to data series using ingest_dayfile.pl script\n";
+  # get today's dayfile and avoid picking up next days dayfile
+  ($today_date)=get_today_date(); 
+  $enddate=$startdate=$today_date;
+
+  #log status to logfile
+  print LF "--->Date range processing are <$startdate> to <$enddate>\n";
+  print LF "--->Start processing day files to data series using ingest_dayfile.pl script\n";
+  close LF;
+
+  #call ingest_dayfile.pl script 
+  $log=`/usr/bin/perl  $script_dir/ingest_dayfile.pl  apidlist=$script_dir/df_apid_list_day_file_hsb start=$startdate end=$enddate dsnlist=$script_dir/df_apid_ds_list_for_hsb src=hsb`;
+
+  #reopen log
+  open(LF,">>$logfile") || die "Can't Open $logfile: $!\n";
+  print LF "--->Completed processing day files to data series using ingest_dayfile.pl script\n";
+
 }
 elsif ($src eq "moc")
 {
@@ -69,11 +86,10 @@ elsif ($src eq "egsefm")
   exit();
 }
 
-#reopen log
-open(LF,">>$logfile") || die "Can't Open $logfile: $!\n";
 #Check if there and then delete all dayfiles that where ingested in dayfile data series 
 open(DELFILE, "$script_dir/DF_DELETE_FILE_LIST") || die "(6)Can't Open $script_dir/DF_DELETE_FILE_LIST file: $!\n";
 @all_del_file_lines="";
+$hkt_filecount =0;
 while (<DELFILE>)
 {
   $_ =~ s/\n//g;
@@ -81,20 +97,26 @@ while (<DELFILE>)
   if ($dflg eq "1") {print LF "validated file saved to drms.deleting file from file system directory:$_\n";}
   $log=`rm $doff_dir/$_`;
   if ($dflg eq "1") {print LF "$log\n";}
-  $hkt_filecount =1;
+  $hkt_filecount++;
 }#while
 if($hkt_filecount > 0)
 {
-  print LF "--->Deleting dayfiles that where successfully processed into data series\n";
+  print LF "--->Deleted dayfiles that where successfully processed into data series. Number deleted:$hkt_filecount\n";
+}
+else
+{
+  print LF "--->Skipping deleting dayfiles because no files ingested to hk_dayfile series\n";
 }
 
 close DELFILE;
+
 #set MF file to blank work was completed
-open(DELFILE, ">$script_dir/DF_DELETE_FILE_LIST") || die "(6)Can't Open $script_dir/DF_HSB_DELETE_FILE_LIST file: $!\n";
+open(DELFILE, ">$script_dir/DF_DELETE_FILE_LIST") || die "(6)Can't Open $script_dir/DF_DELETE_FILE_LIST file: $!\n";
 close DELFILE;
 print LF "--->exiting script getdf.pl\n";
 print LF `date`;
 close LF;
+
 ##############################################################################
 # check_arguments()                                                          #
 ##############################################################################
@@ -123,4 +145,14 @@ sub check_agruments()
      print "Usage: perl getdf.pl  <dayfile source>\nwhere source is either:hsb,moc,egsefm.\n";
      exit;
   }
+}
+
+##############################################################################
+# get_today_date()                                                           #
+##############################################################################
+sub get_today_date
+{
+  ($second, $minute, $hour, $dayOfMonth, $month, $yearOffset, $dayOfWeek, $dayOfYear, $daylightSavings) = localtime();
+  $year = 1900 + $yearOffset;
+  return(sprintf("%-04.4d%-02.2d%-02.2d",$year,$month+1,$dayOfMonth));
 }
