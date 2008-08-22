0b06bf230649e929da1fc3e60778bdffaa338c8c
arta <arta>
2008-08-22 22:51:07 +0000

    Fix build - remove jsoc_info_sock as a make target; jsoc_info.c contains libdrmsserver.a code in it

--- export/apps/Rules.mk
+++ export/apps/Rules.mk
@@ -8,8 +8,11 @@ d		:= $(dir)
 
 # Local variables
 # NOTE: Add the base of the module's filename below (next to mymod)
-MODEXE_$(d)	:= $(addprefix $(d)/, jsoc_export_as_fits jsoc_export_as_is jsoc_info jsoc_fetch jsoc_export_manage jsoc_export_make_index)
-MODEXE		:= $(MODEXE) $(MODEXE_$(d))
+MODEXE_$(d)	:= $(addprefix $(d)/, jsoc_export_as_fits jsoc_export_as_is jsoc_fetch jsoc_export_manage jsoc_export_make_index)
+
+MODEXE_ONLY_$(d)	:= $(addprefix $(d)/, jsoc_info)
+
+MODEXE		:= $(MODEXE) $(MODEXE_$(d)) $(MODEXE_ONLY_$(d))
 # CEXE_$(d)       := $(addprefix $(d)/, GetJsocRequestID jsoc_export_make_index)
 CEXE_$(d)       := $(addprefix $(d)/, GetJsocRequestID)
 CEXE            := $(CEXE) $(CEXE_$(d))
@@ -18,7 +21,7 @@ CEXE            := $(CEXE) $(CEXE_$(d))
 MODEXE_SOCK_$(d):= $(MODEXE_$(d):%=%_sock)
 MODEXE_SOCK	:= $(MODEXE_SOCK) $(MODEXE_SOCK_$(d))
 
-EXE_$(d)        := $(MODEXE_$(d)) $(CEXE_$(d))
+EXE_$(d)        := $(MODEXE_$(d)) $(MODEXE_ONLY_$(d)) $(CEXE_$(d))
 OBJ_$(d)	:= $(EXE_$(d):%=%.o) 
 DEP_$(d)	:= $(OBJ_$(d):%=%.o.d)
 CLEAN		:= $(CLEAN) \
@@ -35,7 +38,9 @@ S_$(d)		:= $(notdir $(EXE_$(d)) $(MODEXE_SOCK_$(d)))
 $(OBJ_$(d)):		$(SRCDIR)/$(d)/Rules.mk
 $(OBJ_$(d)):		CF_TGT := $(CF_TGT) -I$(SRCDIR)/$(d)/../../libs/json -I$(SRCDIR)/$(d)/../libs/util
 $(OBJ_$(d)):		CF_TGT := $(CF_TGT) -DCDIR="\"$(SRCDIR)/$(d)\""
+
 $(MODEXE_$(d)):		$(LIBJSON) $(LIBEXPUTL)
+$(MODEXE_ONLY_$(d)):	$(LIBJSON) $(LIBEXPUTL)
 $(MODEXE_SOCK_$(d)):	$(LIBJSON) $(LIBEXPUTL)
 
 # Shortcuts
