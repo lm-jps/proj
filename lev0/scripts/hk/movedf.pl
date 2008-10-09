1de9a3033fbd0eca2f95c9aacb33c693d2fac351
carl <carl>
2008-10-09 18:10:38 +0000

    movedf.pl:Moves to MOC Day Files to dropoff directory and then loads files to DRMS dayfile series. Updated email message and added Art and Rock to receive notice.

--- lev0/scripts/hk/movedf.pl
+++ lev0/scripts/hk/movedf.pl
@@ -153,12 +153,12 @@ elsif (($hkt_filecount +   $xml_filecount) == 0)
 {
   print LF "--->status:warning got no files to loaded into DRMS\n";
   ##note sent email here if occurs
-  sendEmail("carl\@sun.stanford.edu", "carl\@sun.stanford.edu", "JSOC:WARNING:Ingesting MOC dayfiles: status:no files loaded today", "Received count of hkt files:<$hkt_filecount>.\nReceived count of xml files:<$xml_filecount>\n");
+  sendEmail("carl\@sun.stanford.edu,arta\@sun.stanford.com,rock\@solarpost.stanford.edu", "carl\@sun.stanford.edu", "JSOC:WARNING:Ingesting MOC dayfiles: status:no files loaded today", "Warning Message:\n-->Received count of <$hkt_filecount> hkt files and count of <$xml_filecount> xml files from directory <$pup_dir>.\n-->When executing script </home/production/cvs/JSOC/proj/lev0/scripts/hk/movedf.pl> from cron job.\n"); 
 }
 else
 {
   print LF "--->status:not sure of status but got delcount:$delcount hkt_filecount:$hkt_filecount xml_filecount:$xml_filecount\n";
-  sendEmail("carl\@sun.stanford.edu", "carl\@sun.stanford.edu", "JSOC:WARNING:Ingesting MOC dayfiles: status:not sure of status", "Received count of hkt files:<$hkt_filecount>.\nReceived count of xml files:<$xml_filecount>\nLoad and deleted dayfiles:<$delcount>\n");
+  sendEmail("carl\@sun.stanford.edu", "carl\@sun.stanford.edu", "JSOC:WARNING:Ingesting MOC dayfiles: status:not sure of status", "Warning Message:\n-->Received count of <$hkt_filecount> hkt files and count of <$xml_filecount> xml files from directory <$pup_dir>.\n-->When executing script </home/production/cvs/JSOC/proj/lev0/scripts/hk/movedf.pl> from cron job.\n");
 }
 
 close DELFILE;
