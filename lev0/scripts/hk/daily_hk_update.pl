30333277f83dad5d9ba971336c9ad3721f044108
carl <carl>
2008-11-11 21:03:05 +0000

    daily_hk_update.pl: Added line to update JSOC version number lookup files from cvs.

--- lev0/scripts/hk/daily_hk_update.pl
+++ lev0/scripts/hk/daily_hk_update.pl
@@ -1,7 +1,15 @@
 #!/usr/bin/perl
 # gets latest files from cvs for hk config data for lev0 processing.
 # used to update dcs0, dcs1, and dcs2.
-$ENV{JSOCROOT}="/home/production/cvs/JSOC";
+$ENV{'PATH'}="/usr/local/bin:/bin:/usr/bin:.";
 $ENV{CVSROOT}=":ext:sunroom.stanford.edu:/home/cvsuser/cvsroot";
-system("cd /home/production/cvs/TBL_JSOC/lev0/hk_config_file; cvs update -d  . ");
 
+# update hk config files
+print "..doing cvs update of hk config files\n";
+system("cd /home/production/cvs/TBL_JSOC/lev0/hk_config_file; cvs update -d  . ");
+print "..completed cvs update of hk config files!\n\n\n";
+
+#update jsoc version number lookup files
+print "..doing cvs update of hk jsoc version number lookup files.\n";
+system("cd /home/production/cvs/TBL_JSOC/lev0/hk_jsn_map_file/prod; cvs update -d  . ");
+print "..completed cvs update of hk jsoc version number lookup files!\n";
