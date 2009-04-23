f8662a8e2085b8027bf3d309fb81c5c4012f6a8e
arta <arta>
2009-04-23 19:00:00 +0000

    Fix 2 problems - make the bzero/bscale mismatch message appear only in verbose mode; there is no proj/cookbook/apps

--- cookbook/Rules.mk
+++ cookbook/Rules.mk
@@ -5,8 +5,6 @@ d		:= $(dir)
 
 # Subdirectories. Directory-specific rules are optional here. The
 # order NOT matter.
-dir	:= $(d)/apps
--include		$(SRCDIR)/$(dir)/Rules.mk
 
 # Standard things
 d		:= $(dirstack_$(sp))
