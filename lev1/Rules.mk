d30370e84f135e93650a2ef8285be986e70e7457
carl <carl>
2009-10-14 19:31:41 +0000

    Rules.mk: Added file to make C code in lev1/apps

---
+++ lev1/Rules.mk
@@ -0,0 +1,12 @@
+sp 		:= $(sp).x
+dirstack_$(sp)	:= $(d)
+d		:= $(dir)
+
+# Subdirectories. Directory-specific rules are optional here. The
+# order NOT matter.
+dir	:= $(d)/apps
+-include		$(SRCDIR)/$(dir)/Rules.mk
+
+# Standard things
+d		:= $(dirstack_$(sp))
+sp		:= $(basename $(sp))
