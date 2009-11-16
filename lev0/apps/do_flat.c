c9e6875e48f8319e0df8cb0b357a4b137704b7b1
production <production>
2009-11-16 22:00:00 +0000

    add statistics

--- lev0/apps/do_flat.c
+++ lev0/apps/do_flat.c
@@ -1,8 +1,8 @@
+#define HIMGCFGFILE "/home/production/img_cnfg_ids"
+
 #define XXX_CANT_OPEN_HIMGCFGFILE -1
 #define XXX_CORRUPT_HIMGCFGFILE -2
 #define XXX_CANT_FIND_HIMGCFID -3
-
-#define HIMGCFGFILE "/home/production/img_cnfg_ids"
 #define XXX_INVALID_HIMGCFID -4 
 
 #define MAXHIMGCFGS 4096
@@ -11,6 +11,7 @@ static int overscan_ncols[MAXHIMGCFGS];
 static int overscan_rowstr[MAXHIMGCFGS];
 static int overscan_colstr[MAXHIMGCFGS];
 static int overscan_valid[MAXHIMGCFGS];
+static int dvals[MAXHIMGCFGS];
 
 ///////////////////////////////////
 int get_overscan_info(int himgcfid)
@@ -35,9 +36,9 @@ int get_overscan_info(int himgcfid)
 	    continue;
 	if (id != himgcfid)
 	    continue;
-	if (13 != sscanf(line, "%s%s%s%s%s%s%s%s%s%d%d%d%d", j,j,j,j,j,j,j,j,j,
-		&overscan_nrows[id], &overscan_ncols[id],
-		&overscan_rowstr[id], &overscan_colstr[id]))
+	if (15 != sscanf(line, "%s%s%s%s%s%s%s%s%s%d%d%d%d%s%d", 
+	j,j,j,j,j,j,j,j,j, &overscan_nrows[id], &overscan_ncols[id],
+	&overscan_rowstr[id], &overscan_colstr[id], j, &dvals[id]))
 	    return XXX_CORRUPT_HIMGCFGFILE;
 	found = 1;
 	overscan_valid[himgcfid] = 1;
@@ -55,12 +56,16 @@ int do_flat(LEV0LEV1 *info)
     int i,j,idx,IDX;
     int nr,nc,r1,r2,c1,c2;
     int status;
-    static float dat[MAXPIXELS];
-    float *fdat = info->adataff;
-    float *ddat = info->adatadark;
+    float *flat = info->adataff;
+    float *dark = info->adatadark;
     short *in = info->adata0;
-    short *out = info->adata1;
+    int *out = info->adata1;
     short tmp;
+    float ftmp;
+    double dtmp;
+    double s=0.0, s2=0.0, s3=0.0, s4=0.0, ss;
+    int npix, min, max, medn;
+    static int hist[32768];
 
     if (info->himgcfid < 0 || info->himgcfid >= MAXHIMGCFGS)
       return XXX_INVALID_HIMGCFID;
@@ -85,15 +90,29 @@ int do_flat(LEV0LEV1 *info)
 	c1 = c2 = 4096;
     nc /= 2;
 
+    memset(hist, 0, 4*32768);
+
+    //
+    // apply dark, flat correction quadrant by quadrant
+    // and squeeze out the overscan rows and columns
+    //
+    npix = 0;
     for (i=0; i<r1; ++i) {
 	idx = 4096*i;
 	IDX = idx + 4096*nr + nc;
 	for (j=0; j<c1; ++j) {
 	    tmp = in[idx++];
-	    if (BLANK == tmp)
-		out[IDX++] = BLANK;
+	    //if (DRMS_MISSING_INT == tmp)
+	    if (DRMS_MISSING_SHORT == tmp)
+		out[IDX++] = DRMS_MISSING_INT;
 	    else {
-		out[IDX] = (tmp - ddat[IDX]) / fdat[IDX];
+		ftmp = roundf((tmp - dark[IDX]) / flat[IDX]);
+		if (ftmp >=0 && ftmp < 32768) {
+		    out[IDX] = ftmp;
+		    ++hist[out[IDX]];
+		    ++npix;
+		} else
+		    out[IDX] = DRMS_MISSING_INT;
 		++IDX;
 	    }
 	}
@@ -103,10 +122,17 @@ int do_flat(LEV0LEV1 *info)
 	IDX = idx + 4096*nr - nc;
 	for (j=c2; j<4096; ++j) {
 	    tmp = in[idx++];
-	    if (BLANK == tmp)
-		out[IDX++] = BLANK;
+	    //if (DRMS_MISSING_INT == tmp)
+	    if (DRMS_MISSING_SHORT == tmp)
+		out[IDX++] = DRMS_MISSING_INT;
 	    else {
-		out[IDX] = (tmp - ddat[IDX]) / fdat[IDX];
+		ftmp = roundf((tmp - dark[IDX]) / flat[IDX]);
+		if (ftmp >=0 && ftmp < 32768) {
+		    out[IDX] = ftmp;
+		    ++hist[out[IDX]];
+		    ++npix;
+		} else
+		    out[IDX] = DRMS_MISSING_INT;
 		++IDX;
 	    }
 	}
@@ -116,10 +142,17 @@ int do_flat(LEV0LEV1 *info)
 	IDX = idx - 4096*nr + nc;
 	for (j=0; j<c1; ++j) {
 	    tmp = in[idx++];
-	    if (BLANK == tmp)
-		out[IDX++] = BLANK;
+	    //if (DRMS_MISSING_INT == tmp)
+	    if (DRMS_MISSING_SHORT == tmp)
+		out[IDX++] = DRMS_MISSING_INT;
 	    else {
-		out[IDX] = (tmp - ddat[IDX]) / fdat[IDX];
+		ftmp = roundf((tmp - dark[IDX]) / flat[IDX]);
+		if (ftmp >=0 && ftmp < 32768) {
+		    out[IDX] = ftmp;
+		    ++hist[out[IDX]];
+		    ++npix;
+		} else
+		    out[IDX] = DRMS_MISSING_INT;
 		++IDX;
 	    }
 	}
@@ -129,25 +162,87 @@ int do_flat(LEV0LEV1 *info)
 	IDX = idx - 4096*nr - nc;
 	for (j=c2; j<4096; ++j) {
 	    tmp = in[idx++];
-	    if (BLANK == tmp)
-		out[IDX++] = BLANK;
+	    //if (DRMS_MISSING_INT == tmp)
+	    if (DRMS_MISSING_SHORT == tmp)
+		out[IDX++] = DRMS_MISSING_INT;
 	    else {
-		out[IDX] = (tmp - ddat[IDX]) / fdat[IDX];
+		ftmp = roundf((tmp - dark[IDX]) / flat[IDX]);
+		if (ftmp >=0 && ftmp < 32768) {
+		    out[IDX] = ftmp;
+		    ++hist[out[IDX]];
+		    ++npix;
+		} else
+		    out[IDX] = DRMS_MISSING_INT;
 		++IDX;
 	    }
 	}
     }
 
+    info->datavals = npix;
+    info->missvals = dvals[info->himgcfid] - npix;
+
+    //
+    // fill in blanks around the borders
+    //
     if (nr) {
-	for (i=0; i<4096*nr; ++i) out[i] = BLANK;
-	for (i=4096*(4096-nr); i<MAXPIXELS; ++i) out[i] = BLANK;
+	for (i=0; i<4096*nr; ++i) out[i] = DRMS_MISSING_INT;
+	for (i=4096*(4096-nr); i<MAXPIXELS; ++i) out[i] = DRMS_MISSING_INT;
     }
     if (nc) {
 	for (i=0; i<4096; ++i) {
 	    for (j=0; j<nc; ++j)
-		out[i*4096+j] = BLANK;
+		out[i*4096+j] = DRMS_MISSING_INT;
 	    for (j=4096-nc; j<4096; ++j)
-		out[i*4096+j] = BLANK;
+		out[i*4096+j] = DRMS_MISSING_INT;
+	}
+    }
+
+    //
+    // do statistics
+    //
+    info->datamin = info->datamax = info->datamedn = DRMS_MISSING_INT;
+    //info->datavals = info->missvals = DRMS_MISSING_INT;
+    info->datamean = info->data_rms = info->dataskew 
+	= info->datakurt = DRMS_MISSING_DOUBLE;
+
+    min = 0;
+    max = 32767;
+    if (npix) {
+	while (hist[min] == 0)
+	    ++min;
+	info->datamin = min;
+
+	while (hist[max] == 0)
+	    --max;
+	info->datamax = max;
+
+	medn = min;
+	i = hist[medn];
+	while (2*i < npix)
+	    i += hist[++medn];
+	info->datamedn = medn;
+
+	for (i=min; i<=max; ++i) {
+	    s += (dtmp = i*hist[i]);
+	    s2 += (dtmp *= i);
+	    s3 += (dtmp *= i);
+	    s4 += (dtmp *= i);
+	}
+	s /= npix;
+	info->datamean = s;
+	ss = s*s;
+	s2 /= npix;
+	s3 /= npix;
+	s4 /= npix;
+	if (npix > 1) {
+	    dtmp = npix * (s2-ss) / (npix-1);
+	    info->data_rms = sqrt(dtmp);
+	    if (dtmp > 0.0) {
+		info->dataskew = (s3 - s * (3*s2 - 2*ss)) / 
+		    (dtmp*info->data_rms);
+		info->datakurt = (s4 - 4*s*s3 + 3*ss*(2*s2-ss)) / 
+		    (dtmp*dtmp) - 3;
+	    }
 	}
     }
 
