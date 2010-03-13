bc368e54185f1c9eb7c85701093c18290ada35c1
phil <phil>
2010-03-13 23:30:12 +0000

    Added hg_patch.c

--- util/apps/Rules.mk
+++ util/apps/Rules.mk
@@ -4,7 +4,7 @@ dirstack_$(sp)	:= $(d)
 d		:= $(dir)
 
 # Local variables
-MODEXE_$(d)	:= $(addprefix $(d)/, arithtool rebin2 ingest_from_fits)
+MODEXE_$(d)	:= $(addprefix $(d)/, arithtool rebin2 ingest_from_fits hg_patch)
 MODEXE		:= $(MODEXE) $(MODEXE_$(d)) 
 
 MODEXE_SOCK_$(d):= $(MODEXE_$(d):%=%_sock)
