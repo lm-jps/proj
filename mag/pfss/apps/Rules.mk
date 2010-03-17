ebcbdcb603786b64b6e367b6740e900ce8692f74
xudong <xudong>
2010-03-17 22:29:49 +0000

    Commited first mag module: polar fitting

--- mag/pfss/apps/Rules.mk
+++ mag/pfss/apps/Rules.mk
@@ -5,40 +5,63 @@ d		:= $(dir)
 
 # Local variables
 # NOTE: Add the base of the module's filename below (next to mymod)
-MODEXE_$(d)	:= $(addprefix $(d)/, ) 
+MODEXE_$(d)	:= $(addprefix $(d)/, )
 MODEXE		:= $(MODEXE) $(MODEXE_$(d))
 
 MODEXE_SOCK_$(d):= $(MODEXE_$(d):%=%_sock)
 MODEXE_SOCK	:= $(MODEXE_SOCK) $(MODEXE_SOCK_$(d))
 
-MODEXE_USEF_$(d):= $(RDVINV_$(d))
-MODEXE_USEF	:= $(MODEXE_USEF) $(MODEXE_USEF_$(d))
+# Modules with external libraries
+MODEXE_USEF_$(d)	:= $(addprefix $(d)/, polfil)
+MODEXE_USEF		:= $(MODEXE_USEF) $(MODEXE_USEF_$(d))
 
-EXE_$(d)        := $(MODEXE_$(d)) $(MODEXE_USEF_$(d))
+MODEXE_USEF_SOCK_$(d)	:= $(MODEXE_USEF_$(d):%=%_sock)
+MODEXE_USEF_SOCK	:= $(MODEXE_USEF_SOCK) $(MODEXE_USEF_SOCK_$(d))
+
+EXE_$(d)	:= $(MODEXE_$(d)) $(MODEXE_USEF_$(d))
 OBJ_$(d)	:= $(EXE_$(d):%=%.o) 
 DEP_$(d)	:= $(OBJ_$(d):%=%.d)
 CLEAN		:= $(CLEAN) \
 		   $(OBJ_$(d)) \
 		   $(EXE_$(d)) \
 		   $(MODEXE_SOCK_$(d))\
+		   $(MODEXE_USEF_SOCK_$(d)) \
 		   $(DEP_$(d))
 
-TGT_BIN	        := $(TGT_BIN) $(EXE_$(d)) $(MODEXE_SOCK_$(d))
+TGT_BIN	        := $(TGT_BIN) $(EXE_$(d)) $(MODEXE_SOCK_$(d)) $(MODEXE_USEF_SOCK_$(d))
 
 S_$(d)		:= $(notdir $(EXE_$(d)) $(MODEXE_SOCK_$(d)))
 
 # Local rules
 $(OBJ_$(d)):		$(SRCDIR)/$(d)/Rules.mk
-$(OBJ_$(d)):		CF_TGT := $(CF_TGT) -I/home/jsoc/include -DCDIR="\"$(SRCDIR)/$(d)\""
+$(OBJ_$(d)):		CF_TGT := -I$(SRCDIR)/$(d)/../../libs/astro -I$(SRCDIR)/$(d)/src/ $(FMATHLIBSH) -I$(SRCDIR)/lib_third_party/include
+$(OBJ_$(d)):		CF_TGT := $(CF_TGT) -DCDIR="\"$(SRCDIR)/$(d)\""
+
+MKL     := -lmkl
+
+ifeq ($(COMPILER), icc)
+  NOIPO_$(d)    := -no-ipo
+endif
+
+ifeq ($(JSOC_MACHINE), linux_x86_64)
+  MKL     := $(NOIPO_$(d)) -lmkl_em64t
+endif
 
 ifeq ($(JSOC_MACHINE), linux_ia32)
-  FFTW_$(d) = /home/jsoc/lib/linux-ia32
-endif
-ifeq ($(JSOC_MACHINE), linux_x86_64)
-  FFTW_$(d) = /home/jsoc/lib/linux-x86_64
+  MKL     := $(NOIPO_$(d)) -lmkl_lapack -lmkl_ia32
 endif
 
-$(MODEXE_$(d)) $(MODEXE_SOCK_$(d)):	LL_TGT := $(LL_TGT) -L$(FFTW_$(d)) -lfftw3 -lfftw3f
+SVML_$(d)       :=
+GUIDE_$(d)      :=
+
+ifeq ($(COMPILER), icc)
+  SVML_$(d)     := -lsvml
+  GUIDE_$(d)    := -lguide
+endif
+
+ALL_$(d)	:= $(MODEXE_$(d)) $(MODEXE_SOCK_$(d)) $(MODEXE_USEF_$(d)) $(MODEXE_USEF_SOCK_$(d))
+$(ALL_$(d)) : $(LIBASTRO)
+$(ALL_$(d)) : LL_TGT :=  $(LL_TGT) $(GSLLIBS) $(CFITSIOLIBS) $(FMATHLIBS) $(SVML_$(d)) $(MKL) $(GUIDE_$(d))
 
 # Shortcuts
 .PHONY:	$(S_$(d))
