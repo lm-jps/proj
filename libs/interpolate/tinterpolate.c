01df8886bbef8f3de8f41ce5f26bf5f16e4468d4
schou <schou>
2010-04-10 00:10:18 +0000

    Added temporal averaging

--- libs/interpolate/tinterpolate.c
+++ libs/interpolate/tinterpolate.c
@@ -9,6 +9,7 @@
 #include "drms_defs.h"
 
 #define minval(x,y) (((x) < (y)) ? (x) : (y))
+#define maxval(x,y) (((x) > (y)) ? (x) : (y))
 
 int tinterpolate(
   int nsample, // Number of input times
@@ -72,7 +73,7 @@ int tinterpolate(
   }
   fileptr = fopen (filename,"r");
   if (fileptr==NULL) {
-    printf("File not found in init_fill.\n");
+    printf("File not found in tinterpolate.\n");
     return 1;
   }
   if (method != 0) {
@@ -263,6 +264,308 @@ nblock=1024;
 
   t2=dsecnd()-t2;
   printf("%d %f %f %f\n",nsample,t0,t1,t2);
+
+  MKL_free(acor);
+  MKL_free(a0);
+  MKL_free(a);
+  MKL_free(rh0);
+  MKL_free(coeffd);
+  MKL_free(coeff);
+  MKL_free(a1b);
+  MKL_free(a1r);
+  MKL_free(coeffs);
+  MKL_free(wgood);
+  free (filename);
+  return 0;
+}
+
+
+int taverage(
+  int nsample, // Number of input times
+  double *tsample, // Input times
+  double tint, // Target time
+  int nconst, // Number of polynomial terms exactly reproduced
+  float **images, // Pointer array to input images
+  unsigned char **masks, // Pointer array to input masks. 0=good, 1= missing
+  float *image_out, // Interpolated image
+  int nx, // Number of points in dimension adjacent in memory
+  int ny, // Number of points in dimension not adjacent in memory
+  int nlead, // Leading dimension of arrays. nlead>=nx
+  int method, // Interpolation method
+  char **filenamep, // Pointer to name of file to read covariance from.
+                   // Set to actual file used if method > 0.
+  int avmethod, // averaging method
+  int order, // Interpolation order
+  double cadence, // Cadence to interpolate to
+  int hwidth, // In units of cadence. Total width is 2*hwidth+1
+  double par1 // In units of cadence.
+)
+{
+  const unsigned char maskgood=0; // Value of good entries in masks
+  int malign=32;
+  int ncor=5000;
+  //double dta=1.0; // Time between samples of input autocorrelation
+  int i,j,k,isample;
+  FILE *fileptr;
+  double ti,dt,dtx;
+  double *acor; // Input autocorrelation
+  double *a0,*a,*rh0,*coeffd;
+  double *a1b,bta1b,*a1r,bta1r,c;
+  int idt;
+  int info;
+  char uplo[] = "U";
+  int ione = 1;
+  float *coeff;
+  int nmasks,smask,imask,imask1,ngood;
+  int *wgood;
+  int *smasks;
+  float *coeffs,*cp;
+  float sum,*sums,*ip;
+  double t0,t1,t2;
+  unsigned char *mp;
+  int ix;
+  //int ib, nblock;
+  char *filename;
+
+  if ((order < 1) || (order > 20)) {
+// Code breaks at 31 or 32 due to the use of summed masks
+// Also, memory usage and efficiency drops due to all masks calculated for >20
+    printf("taverage: must have 1<=order<=20\n");
+    return 1;
+  }
+  t0=dsecnd();
+  switch (method) {
+  case 0:
+    filename=strdup(*filenamep);
+    break;
+  case 1:
+    filename=strdup(DEFS_MKPATH("/data/acort_hr12.txt"));
+    break;
+  default:
+    printf("Unknown method in init_fill.\n");
+    return 1;
+    break;
+  }
+  fileptr = fopen (filename,"r");
+  if (fileptr==NULL) {
+    printf("File not found in taverage.\n");
+    return 1;
+  }
+  if (method != 0) {
+    if (filenamep != NULL) {
+      *filenamep=strdup(filename);
+    }
+  }
+
+// Read autocorrelation
+  acor=(double *)(MKL_malloc(ncor*sizeof(double),malign));
+//fread ((char*)(acor),sizeof(double),ncor,fileptr);
+  for (i=0;i<ncor;i++) fscanf(fileptr,"%lf",acor+i);
+  fclose(fileptr);
+  t0=dsecnd()-t0;
+
+int width,*ix1,*ixi,*ihelp,iwidth,iorder;
+double *tw,*tx1,*txi,*thelp,*weights,wsum;
+int imin,imin1,minuse,maxuse;
+double tmin;
+
+// Now find times to do averaging over and interpolate to.
+  width=2*hwidth+1;
+  tw=(double *)(MKL_malloc(width*sizeof(double),malign));
+  weights=(double *)(MKL_malloc(width*sizeof(double),malign));
+  wsum=0.0;
+  for (i=0;i<width;i++) {
+    dt=i-hwidth+0.0;
+    tw[i]=tint+cadence*dt;
+    switch (avmethod) {
+    case tavg_boxcar:
+      weights[i]=1.0;
+      break;
+    case tavg_cosine:
+      weights[i]=0.5*(1+cos(M_PI*dt/hwidth));
+      break;
+    case tavg_fourth:
+      weights[i]=1-dt/hwidth*dt/hwidth;
+      weights[i]=weights[i]*weights[i];
+      break;
+    case tavg_hathaway:
+      weights[i]=(exp(dt*dt/(2*hwidth*hwidth))-exp(par1*par1/(2*hwidth*hwidth)))*(1+(par1*par1-dt*dt)/(2*hwidth*hwidth));
+    default:
+      printf("Unimplemented method in taverage\n");
+      return 1;
+    }
+    wsum=wsum+weights[i];
+  }
+// Normalize weigths
+  for (i=0;i<width;i++) weights[i]=weights[i]/wsum;
+  //for (i=0;i<width;i++) printf("%f\n",weights[i]);
+
+// Get list of (closest) input times to use for each target time.
+  ix1=(int *)(MKL_malloc(order*width*sizeof(int),malign));
+  tx1=(double *)(MKL_malloc(order*width*sizeof(double),malign));
+  thelp=(double *)(MKL_malloc(nsample*sizeof(double),malign));
+  ihelp=(int *)(MKL_malloc(nsample*sizeof(int),malign));
+
+  minuse=nsample; // Minimum index actually used in input
+  maxuse=-1; // Maximum index actually used in input
+  for (i=0;i<width;i++) { // Loop over target times and find closest order times.
+    for (k=0;k<nsample;k++) {
+      ihelp[k]=k;
+      thelp[k]=tsample[k];
+    }
+    for (j=0;j<order;j++) { // Find closest order points.
+      tmin=1e6;
+      for (k=0;k<nsample-j;k++) { // Loop over remaining points and find closest one.
+        if (fabs(thelp[k]-tw[i]) < tmin) {
+          imin=k;
+          tmin=fabs(thelp[k]-tw[i]);
+        }
+      }
+      imin1=ihelp[imin]; // Index into original table
+      minuse=minval(minuse,imin1);
+      maxuse=maxval(maxuse,imin1);
+      ix1[i*order+j]=imin1;
+      tx1[i*order+j]=tsample[imin1];
+      for (k=imin;k<nsample-j-1;k++) { // Remove smallest element.
+        ihelp[k]=ihelp[k+1];
+        thelp[k]=thelp[k+1];
+      }
+    }
+/*
+    printf("%d %f",i,tw[i]);
+    for (j=0;j<order;j++) printf(" %d",ix1[i*order+j]); printf("\n");
+    for (j=0;j<order;j++) printf(" %f",tx1[i*order+j]); printf("\n");
+*/
+  }
+
+  t1=dsecnd();
+  a0=(double *)(MKL_malloc(nsample*nsample*sizeof(double),malign));
+  a=(double *)(MKL_malloc(nsample*nsample*sizeof(double),malign));
+  rh0=(double *)(MKL_malloc(nsample*sizeof(double),malign));
+  coeffd=(double *)(MKL_malloc(nsample*sizeof(double),malign));
+  coeff=(float *)(MKL_malloc(nsample*sizeof(float),malign));
+  a1b=(double *)(MKL_malloc(nsample*sizeof(double),malign));
+  a1r=(double *)(MKL_malloc(nsample*sizeof(double),malign));
+
+  nmasks=1;
+  for (i=0;i<order;i++) nmasks=2*nmasks;
+
+//Coeffs will now hold weights for all target and input times.
+  coeffs=(float *)(MKL_malloc(width*order*nmasks*sizeof(float),malign));
+  wgood=(int *)(MKL_malloc(width*sizeof(int),malign));
+
+  for (iwidth=0;iwidth<width;iwidth++) { // Loop over target times
+    txi=tx1+iwidth*order; // Times for this target
+    for (i=0;i<order;i++) { // Loop over input times for that target
+      ti=txi[i];
+      for (j=0;j<order;j++) {
+        dt=fabs(txi[j]-ti);
+        idt=(float)floor(dt);
+        dtx=dt-idt;
+        a0[i*order+j]=(1.0-dtx)*acor[idt]+dtx*acor[idt+1];
+      }
+      dt=fabs(tw[iwidth]-ti);
+      idt=(float)floor(dt);
+      dtx=dt-idt;
+      rh0[i]=(1.0-dtx)*acor[idt]+dtx*acor[idt+1];
+    }
+
+// For the time being calculate weights for all possible masks
+// Could be done implicitly if order becomes large
+// For 4096x4096 images interpolation dominates to around order=20
+    for (imask=0;imask<nmasks;imask++) {
+//printf("%d %d\n",isample,imask);
+      cp=coeffs+order*imask+iwidth*order*nmasks;
+      imask1=imask;
+      ngood=0;
+// Find good and bad given mask
+      for (i=0;i<order;i++) {
+        if ((imask1&1)==0) { // Good data
+          wgood[ngood]=i;
+          ngood=ngood+1;
+        }
+        imask1=imask1/2;
+        cp[i]=0.0f; // Zero all coefficients
+      } // for i
+      if (ngood>0) {
+// Pick out relevant parts of a0 and rh0
+        for (i=0;i<ngood;i++) {
+          for (j=0;j<ngood;j++) a[ngood*i+j]=a0[order*wgood[i]+wgood[j]];
+          coeffd[i]=rh0[wgood[i]];
+        }
+        dpotrf(uplo,&ngood,a,&ngood,&info); // Cholesky decomposition
+        if (nconst==0) {
+          dpotrs(uplo,&ngood,&ione,a,&ngood,coeffd,&ngood,&info);
+        }
+        else {
+          for (i=0;i<ngood;i++) a1b[i]=1.;
+          dpotrs(uplo,&ngood,&ione,a,&ngood,a1b,&ngood,&info);
+          bta1b=0.;
+          for (i=0;i<ngood;i++) bta1b=bta1b+a1b[i];
+          for (i=0;i<ngood;i++) a1r[i]=rh0[wgood[i]];
+          dpotrs(uplo,&ngood,&ione,a,&ngood,a1r,&ngood,&info);
+          bta1r=0.0;
+          for (i=0;i<ngood;i++) bta1r=bta1r+a1r[i];
+          c=(bta1r-1.)/bta1b;
+          for (i=0;i<ngood;i++) coeffd[i]=a1r[i]-c*a1b[i];
+        } // nconst
+        for (i=0;i<ngood;i++) cp[wgood[i]]=(float)coeffd[i];
+      } // if ngood
+    } // for imask
+/*
+printf("%d",iwidth);
+for (iorder=0;iorder<order;iorder++) printf(" %f",coeffs[iwidth*order*nmasks+iorder]);
+printf("\n");
+*/
+  } // for isample
+  t1=dsecnd()-t1;
+
+  t2=dsecnd();
+
+// Calculate coefficients for case of no missing data.
+  float *coeff0;
+  coeff0=(float *)(MKL_malloc(nsample*sizeof(float),malign));
+  for (isample=0;isample<nsample;isample++) coeff0[isample]=0.0f;
+  for (iwidth=0;iwidth<width;iwidth++) {
+    for (iorder=0;iorder<order;iorder++) {
+      isample=ix1[iwidth*order+iorder];
+      coeff0[isample]=coeff0[isample]+coeffs[iwidth*order*nmasks+iorder]*weights[iwidth];
+    }
+  }
+  //for (isample=0;isample<nsample;isample++) printf(" %f",coeff0[isample]); printf("\n");
+
+// Now do actual interpolation
+// Inner loop over time
+  for (j=0;j<ny;j++) {
+    for (i=0;i<nx;i++) {
+      ix=i+nlead*j; // Index into array
+// First encode mask in single integer
+      smask=0;
+      for (isample=nsample-1;isample>=0;isample--) {
+        if (masks[isample][ix]!=maskgood) smask=smask+1;
+      }
+      sum=0.0f;
+      if (smask==0) { // All are there
+        for (isample=0;isample<nsample;isample++) sum=sum+coeff0[isample]*images[isample][ix];
+      }
+/*
+      else { // Some are missing.
+// Could keep wgood and index instead
+        cp=coeffs+nsample*smask;
+        for (isample=0;isample<nsample;isample++) {
+// This if statement is costly but needed to avoid accessing NaNs
+          if (masks[isample][ix]==maskgood) {
+            sum=sum+cp[isample]*images[isample][ix];
+          }
+        }
+      } // if (smask)
+*/
+      image_out[ix]=sum;
+    } // for i
+  } // for j
+
+  t2=dsecnd()-t2;
+  //printf("%d %f %f %f\n",nsample,t0,t1,t2);
 
   MKL_free(acor);
   MKL_free(a0);
