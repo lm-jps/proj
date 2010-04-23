89d3ff962307a8a1915e41c7fca68e63ec6f61c3
xudong <xudong>
2010-04-23 07:05:36 +0000

    temp fix for build

--- mag/pfss/apps/Rules.mk
+++ mag/pfss/apps/Rules.mk
@@ -12,7 +12,7 @@ MODEXE_SOCK_$(d):= $(MODEXE_$(d):%=%_sock)
 MODEXE_SOCK	:= $(MODEXE_SOCK) $(MODEXE_SOCK_$(d))
 
 # Modules with external libraries
-MODEXE_USEF_$(d)	:= $(addprefix $(d)/, polfil)
+MODEXE_USEF_$(d)	:= $(addprefix $(d)/, )
 MODEXE_USEF		:= $(MODEXE_USEF) $(MODEXE_USEF_$(d))
 
 MODEXE_USEF_SOCK_$(d)	:= $(MODEXE_USEF_$(d):%=%_sock)
