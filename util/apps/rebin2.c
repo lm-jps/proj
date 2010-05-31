12466722de9ec50a220da5791733c5f5fff629ee
phil <phil>
2010-05-31 22:50:06 +0000

    rewrite to make it work. Now takes int scale >0 for enlarging, <0 to reduce. Fixes WCS keywrods and statistics.

--- util/apps/rebin2.c
+++ util/apps/rebin2.c
@@ -15,7 +15,7 @@
    or fractions of 2) as required and gives out a set of output 
    data. The method for avaraging (interpolation) can be specified 
    through the input "mode". The current version handles a simple 
-   boxcar average.
+   boxcar average. If 'scale' < 0 then the input is reduced in size to 1/|scale|.
 
    Make sure you created the appropriate output series before running 
    the program. For example, su_bala.rebin2up.jsd
@@ -39,7 +39,7 @@
    produces images with the resolution of HMI, 4096 X 4096.
 
    @code
-   rebin2 in='mdi.fd_M_96m_lev18[2003.10.20/1d]' out='su_bala.rebin2up' factor=4 mode='simple'
+   rebin2 in='mdi.fd_M_96m_lev18[2003.10.20/1d]' out='su_bala.rebin2up' scale=4 mode='simple'
    @endcode
 
    @par Example:
@@ -47,7 +47,7 @@
    1024 X 1024. Here the input is the HMI images and the output
    is the lower resolution HMI images.
    @code
-   rebin2 in='su_bala.rebin2up[2003.10.20/1d]' out='su_bala.rebin2down' factor=0.25 mode='simple'
+   rebin2 in='su_bala.rebin2up[2003.10.20/1d]' out='su_bala.rebin2down' scale=0.25 mode='simple'
    @endcode
 
    @bug
@@ -58,7 +58,9 @@
    @verbinclude  ./doxygen_moduletemplate.txt
 */
 
+#include "jsoc.h"
 #include "jsoc_main.h"
+#include "fstats.h"
 
 char *module_name = "rebin2";
 
@@ -68,137 +70,42 @@ ModuleArgs_t module_args[] =
 {
      {ARG_STRING, "in", "NOT SPECIFIED",  "Input data series."},
      {ARG_STRING, "out", "NOT SPECIFIED",  "Output data series."},
-     {ARG_FLOAT, "nfactor", "NOTSPECIFIED", "Reduction factor."},
+     {ARG_FLOAT, "scale", "NOTSPECIFIED", "Scale factor. negative for shrink factor"},
      {ARG_STRING, "mode", "simple", "conversion type."},
      {ARG_END}
 };
 
 int DoIt(void)
   {
-  int resoln, factor;
   int status = DRMS_SUCCESS;
   DRMS_RecordSet_t *inRS, *outRS;
   int irec, nrecs;
-  char *inQuery = params_get_str(&cmdparams, "in");
-  char *outQuery = params_get_str(&cmdparams, "out");
-  char *mode = params_get_str(&cmdparams, "mode");
-  float nfactor = params_get_float(&cmdparams, "nfactor");
-  if (nfactor < 1.0)
-    {
-     resoln = 1;
-     factor = (int) (1/nfactor);
+  const char *inQuery = params_get_str(&cmdparams, "in");
+  const char *outSeries = params_get_str(&cmdparams, "out");
+  const char *mode = params_get_str(&cmdparams, "mode");
+  int scale = params_get_int(&cmdparams, "scale");
+  int iscale, factor;
+  float fscale;
+  if (scale < 0.0)
+    { // shrinking
+    iscale = -scale;
+    fscale = 1.0/iscale;
     }
   else
-    {
-     resoln = 0;
-     factor = (int) nfactor;
+    { // enlarging
+    iscale = (int) scale;
+    fscale = scale;
     }
 
   inRS = drms_open_records(drms_env, inQuery, &status);
   if (status || inRS->n == 0)
     DIE("No input data found");
   nrecs = inRS->n;
-  outRS = drms_create_records(drms_env, nrecs, outQuery, DRMS_PERMANENT, &status);
+ if (nrecs == 0)
+    DIE("No records found");
+  outRS = drms_create_records(drms_env, nrecs, (char *)outSeries, DRMS_PERMANENT, &status);
   if (status)
     DIE("Output recordset not created");
-
- switch (resoln){
-   case 1:
-/* reducing the resolution (of HMI data) to that of MDI
-   resoln = 1
-*/ 
-    for (irec=0; irec<nrecs; irec++)
-     {
-      DRMS_Record_t *outRec, *inRec;
-      DRMS_Segment_t *outSeg, *inSeg;
-      DRMS_Array_t *inArray, *outArray;
-      float *inData, *outData;
-      int inx, iny, outx, outy, i, j, nn;
-      float val;
-      float total = 0.;
- 
-      inRec = inRS->records[irec];
-      inSeg = drms_segment_lookupnum(inRec, 0);
-      inArray = drms_segment_read(inSeg, DRMS_TYPE_FLOAT, &status);
-      if (status)
-       {
-        printf(" No data file found, status=%d\n", status);
-        drms_free_array(inArray);
-        continue;
-       }
-      int naxis = inArray->naxis;
-      int inDims[2] = {inArray->axis[0], inArray->axis[1]};
-      int outDims[2] = {inDims[0]/factor, inDims[1]/factor};
-      inData = (float *)inArray->data;
-      outArray = drms_array_create(DRMS_TYPE_FLOAT, naxis, outDims, NULL, &status);
-      outData = (float *)outArray->data;
-
-      if (strcmp(mode, "simple")==0) // setup for better HMI proxy
-       {
-        for (iny = 0; iny < inDims[1]; iny++)
-         {
-          outy = iny/factor;
-          for (inx = 0; inx < inDims[0]; inx++)
-           {
-            outx = inx/factor;
-            for (j = 0; j < factor; j++)
-              for (i = 0; i < factor; i++)
-               {
-                val = inData[inDims[0]*(iny+j) + inx + i];
-                if (!drms_ismissing_float(val))
-                 {
-                  total = total + val; 
-                  nn++;
-                 }
-                else 
-                total = drms_ismissing_float(val); 
-              }       //i loop
-              if (!drms_ismissing_float(total)) 
-                outData[outDims[0]*outy + outx] = total/nn; 
-              else 
-                outData[outDims[0]*outy + outx] = drms_ismissing_float(val); 
-              total = 0.;
-              nn = 0;
-           }         // end of "inx"
-         }           // end of "iny"
-      }             // end of "mode" loop
-    else
-      DIE("invalid conversion mode");
-
-    drms_free_array(inArray);
-
-    // write data file
-    outRec = outRS->records[irec];
-    outSeg = drms_segment_lookupnum(outRec, 0);
-
-    // get info for array from segment
-    outArray->bzero = outSeg->bzero;
-    outArray->bscale = outSeg->bscale;
-    outArray->parent_segment = outSeg;
-
-    status = drms_segment_write(outSeg, outArray, 0);
-    if (status)
-      DIE("problem writing file");
-    drms_free_array(outArray);
-
-    // copy all keywords
-    drms_copykeys(outRec, inRec, 0, kDRMS_KeyClass_Explicit);
- 
-    // Now fixup coordinate keywords
-    // Only CRPIX1,2 and CDELT1,2 should need repair.
-    drms_setkey_double(outRec, "CDELT1", drms_getkey_double(inRec, "CDELT1", NULL)*4);
-    drms_setkey_double(outRec, "CDELT2", drms_getkey_double(inRec, "CDELT2", NULL)*4);
-    drms_setkey_double(outRec, "CRPIX1", (drms_getkey_double(inRec, "CRPIX1", NULL)-1) /4.0 + 1.0);
-    drms_setkey_double(outRec, "CRPIX2", (drms_getkey_double(inRec, "CRPIX2", NULL)-1) /4.0 + 1.0);
-    } //end of "irec" loop
-/*  
-  -----------------  end of resoln 1 ------------------- */
-     printf("completing %d \n", resoln);
-      break;
-    case 0:
-/* --------- increasing the resolation to that of HMI ---------
-   resol = 0
-*/
 
   for (irec=0; irec<nrecs; irec++)
     {
@@ -207,76 +114,104 @@ int DoIt(void)
     DRMS_Array_t *inArray, *outArray;
     float *inData, *outData;
     int inx, iny, outx, outy, i, j;
-
+    float val;
+    int quality=0;
+ 
     inRec = inRS->records[irec];
-    inSeg = drms_segment_lookupnum(inRec, 0);
-    inArray = drms_segment_read(inSeg, DRMS_TYPE_FLOAT, &status);
-    if (status)
+    quality = drms_getkey_int(inRec, "QUALITY", &status);
+    if (status || (!status && quality >= 0))
       {
-      printf(" No data file found, status=%d\n", status);
-      drms_free_array(inArray);
-      continue;
-      }
-      int naxis = inArray->naxis;
-      int inDims[2] = {inArray->axis[0], inArray->axis[1]};
-      int outDims[2] = {inDims[0]*factor, inDims[1]*factor};
-    inData = (float *)inArray->data;
-    outArray = drms_array_create(DRMS_TYPE_FLOAT, naxis, outDims, NULL, &status);
-    outData = (float *)outArray->data;
-
-
-    if (strcmp(mode, "simple")==0) // setup for better HMI proxy
-      {
-       for (iny = 0; iny < inDims[1]; iny++)
+      inSeg = drms_segment_lookupnum(inRec, 0);
+      inArray = drms_segment_read(inSeg, DRMS_TYPE_FLOAT, &status);
+      if (status)
         {
-         outy = factor*iny;
-         for (inx = 0; inx < inDims[0]; inx++)
+        printf(" No data file found but QUALITY not bad, status=%d\n", status);
+        drms_free_array(inArray);
+        continue;
+        }
+  
+      int naxis = inArray->naxis;
+      int in_nx = inArray->axis[0];
+      int in_ny = inArray->axis[1];
+      int in_inc = (scale < 0 ? 1 : iscale);
+      int out_inc = (scale < 0 ? iscale : 1);
+      int out_nx = in_nx * fscale;
+      int out_ny = in_ny * fscale;
+  
+      int outDims[2] = {out_nx, out_ny};
+      inData = (float *)inArray->data;
+      outArray = drms_array_create(DRMS_TYPE_FLOAT, naxis, outDims, NULL, &status);
+      outData = (float *)outArray->data;
+  
+      if (strcmp(mode, "simple")==0) // setup for better HMI proxy
+        {
+        if (scale < 0) // need to average input chunk
           {
-           float val = inData[inDims[0]*iny + inx];
-if (iny==512 && inx == 512) fprintf(stderr,"512,512 = %f\n",val);
-           outx = factor*inx;
-           for (j = 0; j < factor; j++)
-             for (i = 0; i < factor; i++)
-               outData[outDims[0]*(outy+j) + outx + i] = val;
+          for (outy = 0; outy < out_ny; outy += 1)
+            for (outx = 0; outx < out_nx; outx += 1)
+              {
+              double total = 0.0;
+              int nn = 0;
+              for (j = 0; j < in_inc; j += 1)
+                for (i = 0; i < in_inc; i += 1)
+                  {
+                  val = inData[in_nx*(outy*iscale + j) + outx*iscale + i];
+                  if (!drms_ismissing_float(val))
+                      {
+                       total = total + val; 
+                       nn++;
+                      }
+                  }
+              outData[out_nx*outy + outx] = (nn > 0 ? total/nn : DRMS_MISSING_FLOAT); 
+              }
+          }
+        else  // need to replicate input point
+          {
+          for (iny = 0; iny < in_ny; iny += 1)
+            for (inx = 0; inx < in_nx; inx += 1)
+              {
+              val = inData[in_nx*iny + inx];
+              for (j = 0; j < out_inc; j += 1)
+                for (i = 0; i < out_inc; i += 1)
+                  outData[out_nx*(iny*iscale + j) + inx*iscale + i] = val;
+              }
           }
         }
-      }
     else
       DIE("invalid conversion mode");
-
+  
     drms_free_array(inArray);
-
+  
     // write data file
     outRec = outRS->records[irec];
     outSeg = drms_segment_lookupnum(outRec, 0);
 
+    // copy all keywords
+    drms_copykeys(outRec, inRec, 0, kDRMS_KeyClass_Explicit);
+    set_statistics(outSeg, outArray, 1);
+
+    // Now fixup coordinate keywords
+    // Only CRPIX1,2 and CDELT1,2 should need repair.
+    drms_setkey_double(outRec, "CDELT1", drms_getkey_double(inRec, "CDELT1", NULL)/fscale);
+    drms_setkey_double(outRec, "CDELT2", drms_getkey_double(inRec, "CDELT2", NULL)/fscale);
+    drms_setkey_double(outRec, "CRPIX1", (drms_getkey_double(inRec, "CRPIX1", NULL)-0.5) * fscale + 0.5);
+    drms_setkey_double(outRec, "CRPIX2", (drms_getkey_double(inRec, "CRPIX2", NULL)-0.5) * fscale + 0.5);
+    drms_setkey_int(outRec, "SCALE", scale);
+
+  
     // get info for array from segment
     outArray->bzero = outSeg->bzero;
     outArray->bscale = outSeg->bscale;
     outArray->parent_segment = outSeg;
-
+  
     status = drms_segment_write(outSeg, outArray, 0);
     if (status)
       DIE("problem writing file");
     drms_free_array(outArray);
-
-    // copy all keywords
-    drms_copykeys(outRec, inRec, 0, kDRMS_KeyClass_Explicit);
-
-    // Now fixup coordinate keywords
-    // Only CRPIX1,2 and CDELT1,2 should need repair.
-    drms_setkey_double(outRec, "CDELT1", drms_getkey_double(inRec, "CDELT1", NULL)/4.0);
-    drms_setkey_double(outRec, "CDELT2", drms_getkey_double(inRec, "CDELT2", NULL)/4.0);
-    drms_setkey_double(outRec, "CRPIX1", (drms_getkey_double(inRec, "CRPIX1", NULL)-1) *4.0 + 1.0);
-    drms_setkey_double(outRec, "CRPIX2", (drms_getkey_double(inRec, "CRPIX2", NULL)-1) *4.0 + 1.0);
     }
-/*
- ------------------------- end resol = 0 -----------------*/
-     printf("ciompleting %d \n", resoln);
-     break;
-  } //end switch case
+   } //end of "irec" loop
 
   drms_close_records(inRS, DRMS_FREE_RECORD);
   drms_close_records(outRS, DRMS_INSERT_RECORD);
   return (DRMS_SUCCESS);
-} // end of DoIt
+  } // end of DoIt
