979365fb8f2545c0b83b0a2c89a9fb9ff05dde94
production <production>
2010-07-30 15:58:45 +0000

    Richard changes

--- lev0/apps/cosmic_ray.c
+++ lev0/apps/cosmic_ray.c
@@ -1,4 +1,4 @@
-int cosmic_rays(DRMS_Record_t *record, int *image_int, int *badpix, int nbad, const float limit, unsigned char *cosmic,  int *n_cosmic, int nx, int ny)
+int cosmic_rays(DRMS_Record_t *record, float *image, int *badpix, int nbad, int *cosmic, int *n_cosmic, int nx, int ny)
 {
   //fresize: structure obtained from init_fresize_gaussian(&fresizes,stdd,nwd,1);
   //image: input image
@@ -11,38 +11,68 @@ int cosmic_rays(DRMS_Record_t *record, int *image_int, int *badpix, int nbad, co
 
   struct fresize_struct fresizes;
 
-  //constants
-   //initialize
-  static int first_time=1;
 
-  if (first_time)
-    {
-      first_time=0;
+  float coef[5];
+
+
       float stdd=fwhm/2.0/sqrt(2.0*log(2.0)); 
       int nwd=(int)(fwhm*2.0);  //kernel size
       init_fresize_gaussian(&fresizes,stdd,nwd,1);
-    }
+      if (fresize == NULL){printf("can not initialize fresize\n"); exit(EXIT_FAILURE);}
 
+      coef[0]=1.00000;
+      coef[1]=0.0;
+      coef[2]=0.0;
+      coef[3]=0.0;
+      coef[4]=0.0;
 
+    
   int i, j;
   int count;
   float *imhp;
-  imhp=(float *)(MKL_malloc(nx*ny*sizeof(float),malign));
+  imhp=(float *)(malloc(nx*ny*sizeof(float)));
+  if (imhp == NULL){printf("can not allocate memory\n"); exit(EXIT_FAILURE);}
   float sigma;
   float sum;
+  float sig, r, factor;
+  float x0, y0, rad;
+  float rsun_obs, image_scl;
   int nump=nx/cent_frac*ny/cent_frac;
-  int status=1;
+  int status=1, status1, status2;
+  int stat_pos=0;
+  int focid, camid;
 
-  float *image;
-  image=(float *)(MKL_malloc(nx*ny*sizeof(float),malign));
-
-  for (j=0; j<ny; ++j) for (i=0; i<nx; ++i) if (image_int[j*nx+i] == nanval) image[j*nx+i]=NAN; else image[j*nx+i]=(float)image_int[j*nx+i];
-
-  for (i=0; i<nbad; ++i)image[badpix[i]]=NAN; // set bad pixels to NAN
 
   
+  x0=drms_getkey_float(record,X0_MP_key,&status);
+  if (status != 0 || isnan(x0)){stat_pos=1; printf("no center\n");}
+
+  y0=drms_getkey_float(record,Y0_MP_key,&status);
+  if (status != 0 || isnan(y0)){stat_pos=1;printf("no center\n");}
+
+  rsun_obs=drms_getkey_double(record,RSUN_OBS_key,&status1);
+  image_scl=drms_getkey_float(record,IMSCL_MP_key,&status2);
+  if (status1 == 0 && status2 == 0 && !isnan(image_scl) && image_scl > 0.0 && !isnan(rsun_obs) && rsun_obs > 0.0){rad=rsun_obs/image_scl;} else {stat_pos=1;printf("no radius\n"); }
+
+  focid=drms_getkey_int(record,HCFTID_key,&status1);
+  camid=drms_getkey_int(record,HCAMID_key,&status1);
+
+  if (status1 != 0 || status2 != 0){printf("no foc id or camera\n"); exit(EXIT_FAILURE);}
+
+  if ((camid != light_val1 && camid != light_val2) || focid < 1 || focid > 16){stat_pos=1; factor=2.0;} else factor=1;
+
+
+
+ 
+ 
+ 
+  for (i=0; i<nbad; ++i)if (badpix[i] < 0 || badpix[i] >  16777216){printf("invalid pixel coordinate\n"); exit(EXIT_FAILURE);}
+  for (i=0; i<nbad; ++i){image[badpix[i]]=NAN; cosmic[i]=badpix[i];} // set bad pixels to NAN
+
+
    fresize(&fresizes,image, imhp, nx,ny,nx,nx,ny,nx,0,0,NAN);   //convolve with gaussian
-  
+
+
   #pragma omp parallel for private(i,j)
   for (j=0; j<ny; ++j) for (i=0; i<nx; ++i) imhp[j*nx+i]=image[j*nx+i]-imhp[j*nx+i]; //highpass filter
 
@@ -51,7 +81,8 @@ int cosmic_rays(DRMS_Record_t *record, int *image_int, int *badpix, int nbad, co
   sum=0.0;
   count=0;
 
-  //calculate stddev at center 
+
+
 #pragma omp parallel for reduction(+:sum,count) private(i,j)  
   for (j=ny*(cent_frac-1)/cent_frac/2; j<ny*(cent_frac+1)/cent_frac/2; ++j) for (i=nx*(cent_frac-1)/cent_frac/2; i<nx*(cent_frac+1)/cent_frac/2; ++i)
     {
@@ -64,38 +95,52 @@ int cosmic_rays(DRMS_Record_t *record, int *image_int, int *badpix, int nbad, co
 
   //detect everything that stick out more than limit*sigma
   if (count > nump/2) sigma=sqrt(sum/(float)(count)); else sigma=NAN;
+  printf("sigma %f\n", sigma);
   //********************************************************************************************************
 
   //check for outliers
   //*******************************************************************************************************
-  count=0;
+  count=nbad;
+ 
+ 
   if (!isnan(sigma) && sigma > sigmamin && sigma < sigmamax)
     {
-      #pragma omp parallel for reduction(+:count) private(i,j)
+#pragma omp parallel for private(i,j,r)
       for (j=0; j<ny; ++j) for (i=0; i<nx; ++i)
 	{
+
 	  if (!isnan(imhp[j*nx+i]))
 	    {
-	      if (imhp[j*nx+i] > limit*sigma)
+	      if (stat_pos == 0) r=sqrt(pow((float)j-y0,2)+pow((float)i-x0,2))/rad; else r=0.0;
+	      if (r< 0.98){
+		sig=(coef[0]+coef[1]*r+coef[2]*pow(r,2)+coef[3]*pow(r,3)+coef[4]*pow(r,4))*sigma;
+		if (imhp[j*nx+i] > limit*factor*sig || image[j*nx+i] > maxval)
 		{
-		  cosmic[j*nx+i]=1;
+                  #pragma omp critical(detect)
+		  cosmic[count]=j*nx+i;
 		  ++count;
 		}
-	      else
-		{
-		  cosmic[j*nx+i]=0;
-		}
+	      }
 	    }
+	    
+	    
 	}
       status=0;
-    }
+     }
   //********************************************************************************************************
 
   *n_cosmic=count;
 
     
-  MKL_free(imhp);
-  MKL_free(image);
+
+ 
+	
+  free_fresize(&fresizes);
+  free(imhp);
+
+
+
 
   return status;
+
 }
