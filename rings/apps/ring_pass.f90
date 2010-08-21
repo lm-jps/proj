67470b4f28731e9662e0c99ae26fbbe5d7d1d3c7
rick <rick>
2010-08-21 02:17:32 +0000

    added for 5.9

---
+++ rings/apps/ring_pass.f90
@@ -0,0 +1,7 @@
+   module ring_pass
+     implicit none
+     integer, parameter :: sle = 4
+     integer, parameter :: dle = 8
+     real(kind=dle) :: dk, dnu
+     integer :: istk, iendk, ni, nj, npj, v, ntot
+   end module ring_pass
