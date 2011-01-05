004eb89ab76078bca92664e5bbe57ae4b0d8bbb6
arta <arta>
2011-01-05 01:38:23 +0000

    The Rules.mk for the observable code didn't have the dependency between the observable modules and the interpolation libraries quite right. I fixed that. The sock versions of the observable code weren't building. I fixed that.

--- lev1.5_hmi/apps/Rules.mk
+++ lev1.5_hmi/apps/Rules.mk
@@ -8,38 +8,34 @@ d		:= $(dir)
 GSLEXE_$(d)	:= $(addprefix $(d)/, HMI_observables lookup phasemaps HMIfilters Leka HMI_IQUV_averaging smoothing HMI_Simulate_Doppler phasemaps_old HMI_observables2 HMI_IQUV_averaging2 phasemaps2 phasemaps_test phasemaps_FeI phasemaps_test_doublegaussian phasemaps_test_voigt HMI_observables_arta correction_velocities)
 GSLOBJ_$(d)	:= $(GSLEXE_$(d):%=%.o) 
 
-MODEXE_$(d)	:= $(addprefix $(d)/,) 
-MODEXE		:= $(MODEXE) $(MODEXE_$(d)) $(GSLEXE_$(d))
+MODEXE		:= $(MODEXE) $(GSLEXE_$(d))
 
-MODEXE_SOCK_$(d):= $(MODEXE_$(d):%=%_sock)
+GSLEXE_SOCK_$(d):= $(GSLEXE_$(d):%=%_sock)
+MODEXE_SOCK_$(d):= $(GSLEXE_SOCK_$(d))
 MODEXE_SOCK	:= $(MODEXE_SOCK) $(MODEXE_SOCK_$(d))
 
-EXE_$(d)	:= $(MODEXE_$(d)) 
+EXE_$(d)	:= $(GSLEXE_$(d)) 
 OBJ_$(d)	:= $(EXE_$(d):%=%.o) 
 
-DEP_$(d)	:= $(OBJ_$(d):%=%.o.d)
 DEP_$(d)	:= $(OBJ_$(d):%=%.d)
 CLEAN		:= $(CLEAN) \
 		   $(OBJ_$(d)) \
-		   $(GSLOBJ_$(d)) \
 		   $(EXE_$(d)) \
 		   $(MODEXE_SOCK_$(d))\
 		   $(DEP_$(d))
 
-S_$(d)		:= $(notdir $(EXE_$(d)) $(MODEXE_SOCK_$(d)) $(GSLEXE_$(d)))
+S_$(d)		:= $(notdir $(EXE_$(d)) $(MODEXE_SOCK_$(d)))
 
 # Local rules
 $(OBJ_$(d)):		$(SRCDIR)/$(d)/Rules.mk
 $(OBJ_$(d)):           CF_TGT := $(CF_TGT) -I$(SRCDIR)/$(d)/../libs/lev15 -I/home/jsoc/include/ -DCDIR="\"$(SRCDIR)/$(d)\"" -I$(SRCDIR)/$(d)/../../libs/interpolate
 $(GSLOBJ_$(d)):        CF_TGT := $(CF_TGT) -openmp -no-ipo
-$(GSLEXE_$(d)):        LF_TGT := $(LF_TGT) -openmp -no-ipo 
+$(GSLEXE_$(d)) $(GSLEXE_SOCK_$(d)):        LF_TGT := $(LF_TGT) -openmp -no-ipo 
 $(GSLOBJ_$(d)):        CF_TGT := $(CF_TGT) $(GSLH)
-$(GSLOBJ_$(d)):        CF_TGT := $(CF_TGT) -I$(SRCDIR)/$(d) -I$(SRCDIR)/$(d)/../libs/lev15 -I/home/jsoc/include/ -I$(SRCDIR)/$(d)/../../libs/interpolate -DCDIR="\"$(SRCDIR)/$(d)\""
-$(GSLEXE_$(d)):        LL_TGT := $(LL_TGT) $(GSLLIBS) -lpthread -lgsl
-$(GSLEXE_$(d)):        LL_TGT := $(LL_TGT) $(GSLLIBS) -L$(FFTW_LIBS) -l$(FFTW3_LIB) -L/home/jsoc/lib/linux-x86_64 -lfftw3 -L$(JSOCROOT)/_linux_x86_64/proj/libs/interpolate -linterp  -static-intel -lmkl_em64t
+$(GSLEXE_$(d)) $(GSLEXE_SOCK_$(d)):        LL_TGT := $(LL_TGT) $(GSLLIBS) -lpthread -lgsl
+$(GSLEXE_$(d)) $(GSLEXE_SOCK_$(d)):        LL_TGT := $(LL_TGT) $(GSLLIBS) -L$(FFTW_LIBS) -l$(FFTW3_LIB) -L/home/jsoc/lib/linux-x86_64 -lfftw3 -static-intel -lmkl_em64t
 
-$(MODEXE_$(d)) $(MODEXE_SOCK_$(d)):	$(LIBLEV15) $(LIBINTERP)
-$(GSLEXE_$(d)): $(LIBLEV15)
+$(EXE_$(d)) $(MODEXE_SOCK_$(d)):	$(LIBLEV15) $(LIBINTERP)
 
 # Shortcuts
 .PHONY:	$(S_$(d))
