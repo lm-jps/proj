fa7e6fe31df31d0fe7b814e3105f014328bfffae
carl <carl>
2011-02-23 20:41:31 +0000

    added  -h stuff

--- lev0/scripts/hk/check_hsb_dayfile.pl
+++ lev0/scripts/hk/check_hsb_dayfile.pl
@@ -1,9 +1,28 @@
 #!/usr/local/bin/perl
-#hsb_0445_2011_02_20_00_00_02_00.hkt 
+############################################################################ 
+#check_hsb_dayfile.pl is use to show whether to save dayfiles with
+#hsb or hsb_r. This information is then used to run ingest_dayfile.pl
+#to save hsb dayfiles to series.
+#created 2/23/2011
+#created by:carl
+############################################################################ 
 $infile="./hsb.txt";
 $log=`sort ./hsb.txt > ./sort.hsb.txt`;
 $sort_infile="./sort.hsb.txt";
-open(FILE, "$sort_infile") || die "Can't Open: <$sort_infile> file: $!\n";
+#get apid argument
+if (substr($ARGV[0],0,2) eq "-h")
+{
+    print "USAGE: % check_hsb_dayfile.pl\n";
+    print "Requirements:\n";
+    print "(1) Create a hsb.txt file from email containing all files to process and save to ./hsb.txt file\n";
+    print "(2) When editing email of hsb files remove all text except filenames.\n";
+    print "(3) If file not at ./ then won't work unless adjust infile variable at top of script.\n";
+    print "(4) The output can be redirected to file (i,e, check_hsb_dayfile.pl > OUTPUT).\n";
+    print "(5) The output tells you which files to save as \"hsb\" or \"hsb_r\" when using ingest_dayfile.pl script.\n";
+    exit;
+}
+
+open(FILE, "$sort_infile") || die "Can't Open: <$sort_infile> file : $!\n";
 while (<FILE>)
 {
   if(m/hsb_/) 
