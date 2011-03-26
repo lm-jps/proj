9bde143cb6719bb6485d525ae30ac58f7506ac9d
arta <arta>
2011-03-26 23:38:24 +0000

    Make this work on multiple data files; clean up temp table between iterations over data files

--- util/scripts/udsumsforarch.pl
+++ util/scripts/udsumsforarch.pl
@@ -1,6 +1,6 @@
 #!/home/jsoc/bin/linux_x86_64/perl5.12.2 -w
 
-# udsumsforarch.pl jsoc_sums hmidb 5434 production many /home/arta/Projects/SUMS/UpdateSUMSArch/tabdatgroup6_3cols.txt
+# udsumsforarch.pl jsoc_sums hmidb 5434 production many /home/arta/Projects/SUMS/UpdateSUMSArch/filelist.txt
 
 use DBI;
 use DBD::Pg;
@@ -18,17 +18,14 @@ use constant kTQueryOnePerConn => "conn";
 use constant kTQueryOnePerTrans => "xact";
 use constant kTQueryManyPerTrans => "many";
 
-# In raw mode, you cannot collect data on dpshort, dpmid, dplong, ap all at the same time. 
-# This will exhaust machine memory. Need to provide yet another flag to select which ONE
-# of these metrics to obtain - only collect data on a single metric.
-
 my($err);
 
 my($dbname);    # name of the db instance to connect to
 my($dbhost);    # name of the db host on which the db instance resides
 my($dbport);    # port on $dbhost through which connections are made
 my($dbuser);    # database user name (to connect with)
-my($typequery); 
+my($typequery); # one SU per connection, one SU per transaction, or one big query that does all SUs at the same time.
+my($dfilelist); # list of file paths - each file contains data to update sum_main with.
 
 my($dsn);       # database connection arguments
 my($dbh);       # database handle
@@ -49,11 +46,9 @@ $dbhost = $ARGV[1];
 $dbport = $ARGV[2];
 $dbuser = $ARGV[3];
 $typequery = $ARGV[4];
-$datafile = $ARGV[5];
-
+$dfilelist = $ARGV[5];
 
 $err = 0; 
-
 
 if ($typequery eq kTQueryOnePerConn)
 {
@@ -96,11 +91,13 @@ elsif ($typequery eq kTQueryOnePerTrans)
 elsif ($typequery eq kTQueryManyPerTrans)
 {
    # batch sunums together
+   my($group);
+   my($fpath);
    
    if (dbconnect($dbname, $dbhost, $dbport, \$dbh))
    {
-      # Open input file for reading
-      if (open(DATAFILE, "<$datafile"))
+      # Open input file for reading - has 2 columns, group and filepath (path to a file provided by Keh-Cheng)
+      if (open(FILELIST, "<$dfilelist"))
       {
          $stmnt = "CREATE TEMPORARY TABLE arta_updatelist (sunum bigint, loc character varying(80), bytes bigint)";
          ExecStatement(\$dbh, $stmnt, 1, "Unable to create temporary table 'arta_updatelist'.\n");
@@ -108,46 +105,71 @@ elsif ($typequery eq kTQueryManyPerTrans)
          $stmnt = "CREATE INDEX arta_updatelist_idx on arta_updatelist (sunum)";   
          ExecStatement(\$dbh, $stmnt, 1, "Unable to create index on temporary table 'arta_updatelist'.\n");
 
-         $stmnt = "COPY arta_updatelist (sunum, loc, bytes) FROM stdin WITH DELIMITER '|'";
-         ExecStatement(\$dbh, $stmnt, 1, "Troubles copying data to temporary table 'arta_updatelist'.\n");
-
-         # Create a temporary data that holds all update rows - loops through files
-         # provided by Keh-Cheng (one file per tape group).
-         while(defined($line = <DATAFILE>))
+         # loop through filelist
+         while (defined($line = <FILELIST>))
          {
-            $rv = $dbh->pg_putcopydata($line);
-            if (!$rv)
+            chomp($line);
+
+            if ($line =~ /^\#/)
             {
-               print STDERR "Failure sending line '$line' to db.\n";
-               $err = 1;
+               next;
             }
-         }
 
-         if (!$err)
-         {
-            $rv = $dbh->pg_putcopyend();
-            if (!$rv)
+            if ($line =~ /(\S+)\s+(\S+)/)
             {
-               print STDERR "Failure ending table copy.\n";
-               $err = 1;
+               $group = $1;
+               $fpath = $2;
             }
-         }
-
-         if (!$err)
-         {
-            if (0)
+            else
             {
-               # test to see if this worked.
-               $stmnt = "SELECT * from arta_updatelist";
-               $rrows = $dbh->selectall_arrayref($stmnt, undef);
-               $err = !(NoErr($rrows, \$dbh, $stmnt));
+               print STDERR "skipping invalid line $line.\n";
+               next;
+            }
 
-               foreach $row (@$rrows)
+            if (open(DATAFILE, "<$fpath"))
+            {
+               $stmnt = "COPY arta_updatelist (sunum, loc, bytes) FROM stdin WITH DELIMITER '|'";
+               ExecStatement(\$dbh, $stmnt, 1, "Troubles copying data to temporary table 'arta_updatelist'.\n");
+
+               # Create a temporary data that holds all update rows.
+               while (defined($line = <DATAFILE>))
                {
-                  print "row $row->[0], $row->[1], $row->[2]\n";
+                  $rv = $dbh->pg_putcopydata($line);
+                  if (!$rv)
+                  {
+                     print STDERR "Failure sending line '$line' to db.\n";
+                     $err = 1;
+                  }
                }
+               
+               if (!$err)
+               {
+                  $rv = $dbh->pg_putcopyend();
+                  if (!$rv)
+                  {
+                     print STDERR "Failure ending table copy.\n";
+                     $err = 1;
+                  }
+               }
+               
+               close(DATAFILE);
             }
 
+            if (!$err)
+            {
+               if (0)
+               {
+                  # test to see if this worked.
+                  $stmnt = "SELECT * from arta_updatelist";
+                  $rrows = $dbh->selectall_arrayref($stmnt, undef);
+                  $err = !(NoErr($rrows, \$dbh, $stmnt));
+                  
+                  foreach $row (@$rrows)
+                  {
+                     print "row $row->[0], $row->[1], $row->[2]\n";
+                  }
+               }
+               
             # Do a join that will update sum_main with data from arta_updatelist
 
             # XXXXXXXXXXXXX - using a surrogate SELECT statement (instead of the real UPDATE one)
@@ -155,31 +177,41 @@ elsif ($typequery eq kTQueryManyPerTrans)
             # TBD - currently, this uses a select statement to test out run times. The real query
             # will be an update statement.
             # REAL QUERY --> "UPDATE sum_main m SET archive_status = '2', arch_tape = ul.tapeid, arch_tape_fn = ul.fileid, arch_tape_date = 'date string' FROM arta_updatelist ul WHERE (m.ds_index = ul.sunum)"
-            $stmnt = "SELECT m.archive_status, m.arch_tape, m.arch_tape_fn, m.arch_tape_date, ul.sunum, ul.loc FROM arta_updatelist ul JOIN (SELECT ds_index, archive_status, arch_tape, arch_tape_fn, arch_tape_date FROM sum_main where storage_group = 6) m ON (m.ds_index = ul.sunum)";
+               $stmnt = "SELECT m.archive_status, m.arch_tape, m.arch_tape_fn, m.arch_tape_date, ul.sunum, ul.loc FROM arta_updatelist ul JOIN (SELECT ds_index, archive_status, arch_tape, arch_tape_fn, arch_tape_date FROM sum_main where storage_group = $group) m ON (m.ds_index = ul.sunum)";
 
-            $rrows = $dbh->selectall_arrayref($stmnt, undef);
-            $err = !(NoErr($rrows, \$dbh, $stmnt));
+               $rrows = $dbh->selectall_arrayref($stmnt, undef);
+               $err = !(NoErr($rrows, \$dbh, $stmnt));
 
-            if (kDEBUG)
-            {
-               my($col);
-               my(@outd);
-               my($ridx);
-
-               foreach $row (@$rrows)
+               if (!$err)
                {
-                  $ridx = 0;
-
-                  while ($ridx < scalar(@$row))
+                  if (kDEBUG)
                   {
-                     $outd[$ridx] = (defined($row->[$ridx]) && length($row->[$ridx]) > 0) ? $row->[$ridx] : 0;
-                     $ridx++;
-                  }
+                     my($col);
+                     my(@outd);
+                     my($ridx);
 
-                  print "row $outd[0], $outd[1], $outd[2], $outd[3], $outd[4], $outd[5]\n";
+                     foreach $row (@$rrows)
+                     {
+                        $ridx = 0;
+
+                        while ($ridx < scalar(@$row))
+                        {
+                           $outd[$ridx] = (defined($row->[$ridx]) && length($row->[$ridx]) > 0) ? $row->[$ridx] : 0;
+                           $ridx++;
+                        }
+
+                        print "row $outd[0], $outd[1], $outd[2], $outd[3], $outd[4], $outd[5]\n";
+                     }
+                  }
                }
             }
-         }
+            
+            # drop rows from temp table
+            $stmnt = "DELETE from arta_updatelist";
+            ExecStatement(\$dbh, $stmnt, 1, "Couldn't drop rows from temporary table 'arta_updatelist'.\n");
+         } # loop over data files
+
+         close(FILELIST);
       }
 
       $dbh->disconnect();
