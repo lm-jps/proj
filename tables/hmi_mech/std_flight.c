46b1683e6bdb47b382f697f593a0cc6cc901ef74
production <production>
2011-07-11 21:54:43 +0000

    initial

---
+++ tables/hmi_mech/std_flight.c
@@ -0,0 +1,22 @@
+#! $Id: std_flight.c,v 1.1 2011/07/11 21:54:43 production Exp $
+#! $Name:  $
+#! Description: Standard Cal/Focus Table
+
+#ID CF1 CF2 
+ 1  28  31   # CF1=BLANK,    	CF2= BLANK
+ 2  28 103   # CF1=BLANK,     	CF2= 0.85 mm	  
+ 3  28 139   # CF1=BLANK,	CF2= 1.7 mm
+ 4  28 175   # CF1=BLANK,	CF2= 2.55 mm
+ 5 100  31   # CF1= 3.4 mm,  	CF2= BLANK
+ 6 100 103   # CF1= 3.4 mm, 	CF2= 0.85  mm
+ 7 100 139   # CF1= 3.4 mm, 	CF2= 1.7 mm
+ 8 100 175   # CF1= 3.4 mm, 	CF2= 2.55 mm
+ 9 172  31   # CF1= 6.8 mm,	CF2= BLANK		OBS (Nominal focus)
+10 172 103   # CF1= 6.8 mm,	CF2= 0.85 mm
+11 172 139   # CF1= 6.8 mm,	CF2= 1.7 mm
+12 172 175   # CF1= 6.8 mm,	CF2= 2.55 mm
+13  64  31   # CF1=10.2 mm,	CF2= BLANK
+14  64 103   # CF1=10.2 mm,	CF2= 0.85 mm
+15  64 139   # CF1=10.2 mm,	CF2= 1.7 mm
+16  64 175   # CF1=10.2 mm,	CF2= 2.55 mm thick focus block
+17 136  67   # CF1= 6.8 mm lens, CF2= 5    mm lens 	CAL (calibration mode)
