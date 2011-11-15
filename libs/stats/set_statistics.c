bd3fb7d796e01ff7fa4aaa337f17f914e62bef89
phil <phil>
2011-11-15 20:20:38 +0000

    Added check for TOTVALS either as per record or per segment, uses for total of datavals and missvals.

--- libs/stats/set_statistics.c
+++ libs/stats/set_statistics.c
@@ -1,4 +1,4 @@
-#ident "$Header: /home/akoufos/Development/Testing/jsoc-4-repos-0914/JSOC-mirror/JSOC/proj/libs/stats/set_statistics.c,v 1.3 2010/07/21 00:47:35 phil Exp $"
+#ident "$Header: /home/akoufos/Development/Testing/jsoc-4-repos-0914/JSOC-mirror/JSOC/proj/libs/stats/set_statistics.c,v 1.4 2011/11/15 20:20:38 phil Exp $"
 
 #include <jsoc.h>
 #include <jsoc_main.h>
@@ -21,29 +21,53 @@
  // If the keyword scope is per segment, then the appropriate segment specific
  // keywords will be set.
 
+#define SETKEY(str,val) 				\
+  {							\
+  if (thiskey = drms_keyword_lookup(rec, str, 0))	\
+    drms_setkey_double(rec, str, val);  		\
+  else							\
+    {							\
+    char name[DRMS_MAXNAMELEN];				\
+    strcpy(name, str);	 				\
+	    strcat(name, segstr); 				\
+    if ((thiskey = drms_keyword_lookup(rec, name, 0)) && ((thiskey->info->kwflags & 1) == kKeywordFlag_PerSegment))      \
+        drms_setkey_double(rec, name, val); 		\
+    }                                                 \
+  }
+
 int set_statistics(DRMS_Segment_t *seg, DRMS_Array_t *data, int mode)
   {
   int status;
   DRMS_Record_t *rec;
-  int i, n, nok, segnum, nsegs;
+  int i, n, nok, ntot, segnum, nsegs;
   double min, max, medn, mean, sig, skew, kurt;
   char segstr[10];
   DRMS_Keyword_t *thiskey;
 
-
-  if (!seg || !data) return(DRMS_ERROR_INVALIDDATA);
+  if (!seg || !data)
+    return(DRMS_ERROR_INVALIDDATA);
   rec = seg->record;
-  if (!rec) return(DRMS_ERROR_INVALIDDATA);
+  if (!rec)
+    return(DRMS_ERROR_INVALIDDATA);
   nsegs = rec->segments.num_total;
   segnum = seg->info->segnum;
 
-  if (nsegs>1)
-    sprintf(segstr, "_%03d", segnum);
-  else
-    segstr[0] = '\0';
+  sprintf(segstr, "_%03d", segnum);
 
   for (n=1, i=0; i<data->naxis; i++)
     n *= data->axis[i];
+
+  ntot = drms_getkey_int(rec, "TOTVALS", &status);
+  if (status)
+    {
+    char name[DRMS_MAXNAMELEN];
+    strcpy(name, "TOTVALS");
+    strcat(name, segstr);
+    if ((thiskey = drms_keyword_lookup(rec, name, 0)) && ((thiskey->info->kwflags & 1) == kKeywordFlag_PerSegment))
+        ntot = drms_getkey_int(rec, name, &status);
+    if (status) 
+      ntot = n;
+    }
 
   if (data->type == DRMS_TYPE_FLOAT)
     {
@@ -64,20 +88,6 @@ int set_statistics(DRMS_Segment_t *seg, DRMS_Array_t *data, int mode)
   if (status)
     return(status);
 
-#define SETKEY(str,val) 				\
-  {							\
-  if (thiskey = drms_keyword_lookup(rec, str, 0))	\
-    drms_setkey_double(rec, str, val);  		\
-  else							\
-    {							\
-    char name[DRMS_MAXNAMELEN];				\
-    strcpy(name, str);	 				\
-    strcat(name, segstr); 				\
-    if ((thiskey = drms_keyword_lookup(rec, name, 0)) && ((thiskey->info->kwflags & 1) == kKeywordFlag_PerSegment))      \
-        drms_setkey_double(rec, name, val); 		\
-    }                                                 \
-  }
-
   SETKEY("DATAMEAN", mean);
   SETKEY("DATAMIN", min);
   SETKEY("DATAMAX", max);
@@ -86,6 +96,6 @@ int set_statistics(DRMS_Segment_t *seg, DRMS_Array_t *data, int mode)
   SETKEY("DATASKEW", skew);
   SETKEY("DATAKURT", kurt);
   SETKEY("DATAVALS", (double)nok);
-  SETKEY("MISSVALS", (double)(n - nok));
+  SETKEY("MISSVALS", (double)(ntot - nok));
   return(0);
   }
