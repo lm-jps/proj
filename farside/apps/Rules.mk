34db7c3af0dd3b199cf648eab79a619fd5ee16cb
arta <arta>
2011-11-28 22:31:02 +0000

    Add drms_rebin to list of modules that are generated by the make system.

--- farside/apps/Rules.mk
+++ farside/apps/Rules.mk
@@ -6,10 +6,15 @@ d		:= $(dir)
 # Local variables
 # NOTE: Add the base of the module's filename below (next to mymod)
 
-# Local rules
-$(OBJ_$(d)):		$(SRCDIR)/$(d)/Rules.mk
-$(OBJ_$(d)):		CF_TGT := $(CF_TGT) -I/home/jsoc/include -DCDIR="\"$(SRCDIR)/$(d)\""
+MODEXE_$(d)	:= $(addprefix $(d)/, drms_rebin) 
+MODEXE		:= $(MODEXE) $(MODEXE_$(d))
 
+MODEXE_SOCK_$(d):= $(MODEXE_$(d):%=%_sock)
+MODEXE_SOCK	:= $(MODEXE_SOCK) $(MODEXE_SOCK_$(d))
+
+EXE_$(d)        := $(MODEXE_$(d))
+
+OBJ_$(d)	:= $(EXE_$(d):%=%.o)
 DEP_$(d)	:= $(OBJ_$(d):%=%.d)
 CLEAN		:= $(CLEAN) \
 		   $(OBJ_$(d)) \
@@ -20,6 +25,22 @@ CLEAN		:= $(CLEAN) \
 TGT_BIN	        := $(TGT_BIN) $(EXE_$(d)) $(MODEXE_SOCK_$(d))
 
 S_$(d)		:= $(notdir $(EXE_$(d)) $(MODEXE_SOCK_$(d)))
+
+# Local rules
+$(OBJ_$(d)):		$(SRCDIR)/$(d)/Rules.mk
+$(OBJ_$(d)):		CF_TGT := $(CF_TGT) -I/home/jsoc/include -DCDIR="\"$(SRCDIR)/$(d)\""
+
+
+ifeq ($(JSOC_MACHINE), linux_ia32)
+  FFTW_$(d) = /home/jsoc/lib/linux-ia32
+endif
+ifeq ($(JSOC_MACHINE), linux_x86_64)
+  FFTW_$(d) = /home/jsoc/lib/linux-x86_64
+endif
+
+$(MODEXE_$(d)) $(MODEXE_SOCK_$(d)):	LL_TGT := $(LL_TGT) -L$(FFTW_$(d)) -lfftw3 -lfftw3f
+
+
 
 # Shortcuts
 .PHONY:	$(S_$(d))
