4cd68d24b56298b803a71fc2a76b68f7f078db28
arta <arta>
2011-11-28 18:48:24 +0000

    The Rules.mk for the farside project was causing a warning to print during make because it was including a variable that was undefined. This Rules.mk still doesnt do anything - it is a no-op.

--- farside/apps/Rules.mk
+++ farside/apps/Rules.mk
@@ -10,6 +10,16 @@ d		:= $(dir)
 $(OBJ_$(d)):		$(SRCDIR)/$(d)/Rules.mk
 $(OBJ_$(d)):		CF_TGT := $(CF_TGT) -I/home/jsoc/include -DCDIR="\"$(SRCDIR)/$(d)\""
 
+DEP_$(d)	:= $(OBJ_$(d):%=%.d)
+CLEAN		:= $(CLEAN) \
+		   $(OBJ_$(d)) \
+		   $(EXE_$(d)) \
+		   $(MODEXE_SOCK_$(d))\
+		   $(DEP_$(d))
+
+TGT_BIN	        := $(TGT_BIN) $(EXE_$(d)) $(MODEXE_SOCK_$(d))
+
+S_$(d)		:= $(notdir $(EXE_$(d)) $(MODEXE_SOCK_$(d)))
 
 # Shortcuts
 .PHONY:	$(S_$(d))
