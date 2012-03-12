9841f80992027e166c61e7c7fe2e0ad8488b75ca
kehcheng <kehcheng>
2012-03-12 21:12:56 +0000

    Fix image corruption due to bad skip value in crop table in Dec 2011 - Jan 2012

---
+++ lev0/apps/do_patch.c
@@ -0,0 +1,14 @@
+// crop table corruption Dec 2011 - Jan 2012
+// skip value for row 1065 in quadrant E (lower left)
+// was 512 pixels too large.  Need to shift whole row
+// right by 512 pixels and fill in MISSING on the left
+
+void do_patch1(IMG *img)
+{
+    short *p = &(img->dat[4096*1065]);
+    short tmp[2048];
+    int i;
+    for (i=0; i<512; ++i) tmp[i] = DRMS_MISSING_SHORT;
+    for (i=512; i<2048; ++i) tmp[i] = p[i-512];
+    memcpy(p, tmp, 4096);
+}
