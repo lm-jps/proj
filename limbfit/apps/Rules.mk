e315533088076acfc3120c75178ad3d1ab7f6f6e
arta <arta>
2012-05-23 21:32:21 +0000

    Broken build - there was a rule for making the socket-connect version of the lfwrp module, but that modules code uses server-only (direct-connect-only) code. So I had to elimate the build of the sock version.

--- limbfit/apps/Rules.mk
+++ limbfit/apps/Rules.mk
@@ -8,8 +8,8 @@ MODEXE_USEF_$(d)	:= $(addprefix $(d)/, lfwrp)
 SUPPOBJ_$(d)		:= $(addprefix $(d)/, limbfit do_one_limbfit limb expmax expfit nrutil indexx sort)
 
 MODEXE_USEF 		:= $(MODEXE_USEF) $(MODEXE_USEF_$(d))
-MODEXE_USEF_SOCK_$(d)	:= $(MODEXE_USEF_$(d):%=%_sock)
-MODEXE_USEF_SOCK	:= $(MODEXE_USEF_SOCK) $(MODEXE_USEF_SOCK_$(d))
+#MODEXE_USEF_SOCK_$(d)	:= $(MODEXE_USEF_$(d):%=%_sock)
+#MODEXE_USEF_SOCK	:= $(MODEXE_USEF_SOCK) $(MODEXE_USEF_SOCK_$(d))
 SUPPOBJ_$(d)		:= $(SUPPOBJ_$(d):%=%.o)
 
 OBJ_$(d)		:= $(MODEXE_USEF_$(d):%=%.o) $(SUPPOBJ_$(d))
@@ -19,12 +19,14 @@ DEP_$(d)		:= $(OBJ_$(d):%=%.d)
 CLEAN			:= $(CLEAN) \
 			   $(OBJ_$(d)) \
 			   $(MODEXE_USEF_$(d)) \
-			   $(MODEXE_USEF_SOCK_$(d)) \
+#			   $(MODEXE_USEF_SOCK_$(d)) \
 			   $(DEP_$(d))
 
-TGT_BIN	        := $(TGT_BIN) $(MODEXE_USEF_$(d)) $(MODEXE_USEF_SOCK_$(d))
+#TGT_BIN	        := $(TGT_BIN) $(MODEXE_USEF_$(d)) $(MODEXE_USEF_SOCK_$(d))
+TGT_BIN	        := $(TGT_BIN) $(MODEXE_USEF_$(d)) 
 
-S_$(d)			:= $(notdir $(MODEXE_USEF_$(d)) $(MODEXE_USEF_SOCK_$(d)))
+#S_$(d)			:= $(notdir $(MODEXE_USEF_$(d)) $(MODEXE_USEF_SOCK_$(d)))
+S_$(d)			:= $(notdir $(MODEXE_USEF_$(d)))
 
 # Local rules
 $(OBJ_$(d)):		$(SRCDIR)/$(d)/Rules.mk
@@ -32,10 +34,10 @@ $(OBJ_$(d)):		CF_TGT := $(CF_TGT) -DCDIR="\"$(SRCDIR)/$(d)\"" -I/home/jsoc/inclu
 
 
 $(MODEXE_USEF_$(d)):		LL_TGT := $(LL_TGT) -L/home/jsoc/lib/linux_x86_64/ -lgsl -lgslcblas
-$(MODEXE_USEF_SOCK_$(d)):	LL_TGT := $(LL_TGT) -L/home/jsoc/lib/linux_x86_64/ -lgsl -lgslcblas
+#$(MODEXE_USEF_SOCK_$(d)):	LL_TGT := $(LL_TGT) -L/home/jsoc/lib/linux_x86_64/ -lgsl -lgslcblas
 
 $(MODEXE_USEF_$(d)):		$(SUPPOBJ_$(d))
-$(MODEXE_USEF_SOCK_$(d)):	$(SUPPOBJ_$(d))
+#$(MODEXE_USEF_SOCK_$(d)):	$(SUPPOBJ_$(d))
 
 # Shortcuts
 .PHONY:	$(S_$(d))
