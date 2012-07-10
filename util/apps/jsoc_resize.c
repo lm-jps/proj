cdd960930b8aaf7da4c70944ea6a4d284687e4a6
phil <phil>
2012-07-10 21:58:17 +0000

    Added upNcenter and crop option for HMI

--- util/apps/jsoc_resize.c
+++ util/apps/jsoc_resize.c
@@ -20,6 +20,8 @@ static int hist[NBINS];
 int image_magrotate(void *, int nin, int min, int data_type_input, float theta, float mag,
     float dx, float dy, void **outarray, int *nx, int *ny, int regridtype_input, int stretch_lines);
 
+int drms_appendcomment(DRMS_Record_t *rec, const char *comment, int nl);
+
 ModuleArgs_t module_args[] =
 {
   {ARG_STRING, "in", NOT_SPECIFIED, "Input series query"},
@@ -31,13 +33,56 @@ ModuleArgs_t module_args[] =
   {ARG_DOUBLE, "scale_to", "0.6", "rescale to new CDELT scale"},
   {ARG_STRING, "do_stretchmarks", "0", "replicate pixels created else use missing value"},
   {ARG_STRING, "requestid", NOT_SPECIFIED, "RequestID if used for export"},
-  {ARG_FLAG, "c", "0", "Center Sun in frame, else use first image as reference"},
+  {ARG_FLAG, "c", "0", "crop image to RSUN_OBS-1/4 pixel"},
   {ARG_FLAG, "h", "0", "Print usage message and quit"},
   {ARG_FLAG, "v", "0", "verbose flag"},
   {ARG_END}
 };
 
 char *module_name = "jsoc_resize";
+
+
+// Definitions at top of file
+// ---------------------------------------------------------------------
+
+#define     Deg2Rad    (M_PI/180.0)
+#define     Rad2arcsec (3600.0/Deg2Rad)
+#define     arcsec2Rad (Deg2Rad/3600.0)
+#define     Rad2Deg    (180.0/M_PI)
+
+struct ObsInfo_struct
+  {
+  // from observation info
+  TIME  t_obs;
+  double rsun_obs, obs_vr, obs_vw, obs_vn;
+  double crpix1, crpix2, cdelt1, cdelt2, crota2;
+  double crval1, crval2;
+  double cosa, sina;
+  double obs_b0;
+  // observed point
+  int i,j;
+  // parameters for observed point
+  double x,y,r;
+  double rho;
+  double lon;
+    double lat;
+  double sinlat, coslat;
+  double sig;
+  double mu;
+  double chi;
+  double obs_v;
+  };
+
+typedef struct ObsInfo_struct ObsInfo_t;
+
+void rebinArraySF(DRMS_Array_t *out, DRMS_Array_t *in);
+int upNcenter(DRMS_Array_t *arr, ObsInfo_t *ObsLoc);
+int crop_image(DRMS_Array_t *arr, ObsInfo_t *ObsLoc);
+char *get_input_recset(DRMS_Env_t *drms_env, char *in, TIME cadence);
+
+ObsInfo_t *GetObsInfo(DRMS_Segment_t *seg, ObsInfo_t *pObsLoc, int *rstatus);
+
+// ---------------------------------------------------------------------
 
 int verbose; 
 
@@ -102,7 +147,10 @@ int DoIt ()
   int regridtype = cmdparams_get_int(&cmdparams, "regrid", NULL);
   int center_to = cmdparams_get_int(&cmdparams, "center_to", NULL);
   int do_stretchmarks = cmdparams_get_int(&cmdparams, "do_stretchmarks", NULL);
+  int do_crop = cmdparams_get_int(&cmdparams, "c", NULL) != 0;
+
   double scale_by;
+  char comment[4096];
 
   if (strstr(dsinp, "aia")) is_aia = 1;
   inprs = drms_open_records(drms_env, dsinp, &status);
@@ -110,13 +158,14 @@ int DoIt ()
   drms_stage_records(inprs, 1, 0);
   nrecs = inprs->n;
   printf("%d records\n", nrecs);
-float usedx = 0;
-float usedy = 0;
-float usemag = 1.0;
+  float usedx = 0;
+  float usedy = 0;
+  float usemag = 1.0;
   for (irec=0; irec<nrecs; irec++)
     {
     int quality;
     TIME t_rec, t_obs;
+    ObsInfo_t *ObsLoc;
 
     inprec = inprs->records[irec];
     t_obs = drms_getkey_time(inprec, "T_OBS", &status); if (status) DIE("T_OBS not found!");
@@ -131,6 +180,8 @@ float usemag = 1.0;
         continue;
       }
     quality = drms_getkey_int(inprec, "QUALITY", &status);
+    if (verbose)
+      fprintf(stderr,"rec %d of %d, quality=%X\n",irec,nrecs,quality);
     if (quality < 0)
       continue;
 
@@ -213,6 +264,16 @@ float usemag = 1.0;
             fval[iy*n + n - 1] = 0.0;
             }
           }
+        else
+          {
+          ObsLoc = GetObsInfo(inpseg, NULL, &status);
+          upNcenter(inparr, ObsLoc);
+          if (do_crop)
+            crop_image(inparr, ObsLoc);
+          crota2 = ObsLoc->crota2;
+          crpix1 = ObsLoc->crpix1;
+          crpix2 = ObsLoc->crpix2;
+          }
 
         if (center_to == 0)
           {
@@ -235,6 +296,17 @@ float usemag = 1.0;
         status = image_magrotate( (float *)inparr->data, n, m, dtyp, crota2,
                  mag, dx, dy, &output_array, &nx, &ny,
 		 regridtype, do_stretchmarks);
+        if (verbose)
+          {
+          fprintf(stderr,"image_magrotate called with: "
+            "data=%p, n=%d, m=%d, dtyp=%d, crota2=%f, mag=%f, dx=%f, dy=%f, regridtype=%d, do_stretch=%d\n",
+            (float *)inparr->data, n, m, dtyp, crota2, mag, dx, dy,regridtype, do_stretchmarks);
+          fprintf(stderr,"image_magrotate returned: "
+            "into out=%p, nx=%d, ny=%d, status=%d\n",  output_array, nx, ny, status);
+          }
+        sprintf(comment,"resize: scale_to=%f, mag=%f, dx=%f, dy=%f, regridtype=%d, do_stretch=%d",
+          scale_to, mag, dx, dy,regridtype, do_stretchmarks);
+        drms_appendcomment(outrec, comment, 0);
         if (status) DIE("image_magrotate failed!");
         outdims[0] = nx;
         outdims[1] = ny;
@@ -257,8 +329,303 @@ float usemag = 1.0;
         if (inparr) drms_free_array(inparr);
         if (outarr) drms_free_array(outarr);
         }
+      else
+        {
+        if (verbose)
+          fprintf(stderr,"WARNING - only first segment will be processed.\n");
+        }
       drms_close_record(outrec, DRMS_INSERT_RECORD);
       }
     }
   return 0;
 }
+
+
+
+// definitions at bottom of file
+
+// ----------------------------------------------------------------------
+
+/* center whith whole pixel shifts and rotate by 180 if needed */
+/* Only apply center if it will not result in an image crop.  I.e. not ever
+   for AIA, and not for HMI or MDI or other if a shift of more than 20 arcsec
+   is implied  */
+int upNcenter(DRMS_Array_t *arr, ObsInfo_t *ObsLoc)
+  {
+  int nx, ny, ix, iy, i, j, xoff, yoff, max_off;
+  double rot, x0, y0, mid;
+  float *data;
+  if (!arr || !ObsLoc)
+    return(1);
+  data = arr->data;
+  nx = arr->axis[0];
+  ny = arr->axis[1];
+  x0 = ObsLoc->crpix1 - 1;
+  y0 = ObsLoc->crpix2 - 1;
+  mid = (nx-1.0)/2.0;
+  if ((rot = fabs(ObsLoc->crota2)) > 179 && rot < 181)
+    {
+    // rotate image by 180 degrees by a flip flip
+    float val;
+    int half = nx / 2;
+    int odd = nx & 1;
+    if (odd) half++;
+    for (iy=0; iy<half; iy++)
+      {
+      for (ix=0; ix<nx; ix++)
+        {
+        i = iy*nx + ix;
+        j = (ny - 1 - iy)*nx + (nx - 1 - ix);
+        val = data[i];
+        data[i] = data[j];
+        data[j] = val;
+        }
+      }
+    x0 = nx - 1 - x0;
+    y0 = ny - 1 - y0;
+    rot = ObsLoc->crota2 - 180.0;
+    if (rot < -90.0) rot += 360.0;
+    ObsLoc->crota2 = rot;
+    }
+  // Center to nearest pixel - if OK to do so
+  xoff = round(x0 - mid);
+  yoff = round(y0 - mid);
+  max_off = 20.0 / ObsLoc->cdelt1;
+  if (arr->parent_segment &&
+      arr->parent_segment->record &&
+      arr->parent_segment->record->seriesinfo && 
+      arr->parent_segment->record->seriesinfo->seriesname && 
+      strncasecmp(arr->parent_segment->record->seriesinfo->seriesname, "aia", 3) &&
+      abs(xoff) < max_off && abs(yoff) < max_off) 
+    {
+    if (abs(xoff) >= 1)
+      {
+      for (iy=0; iy<ny; iy++)
+        {
+        float valarr[nx];
+        for (ix=0; ix<nx; ix++)
+          {
+          int jx = ix + xoff;
+          if (jx < nx && jx >= 0)
+            valarr[ix] = data[iy*nx + jx];
+          else
+            valarr[ix] = DRMS_MISSING_FLOAT;
+          }
+        for (ix=0; ix<nx; ix++)
+          data[iy*nx + ix] = valarr[ix];
+        }
+      x0 -= xoff;
+      }
+    if (abs(yoff) >= 1)
+      {
+      for (ix=0; ix<nx; ix++)
+        {
+        float valarr[ny];
+        for (iy=0; iy<ny; iy++)
+          {
+          int jy = iy + yoff;
+          if (jy < ny && jy >= 0)
+            valarr[iy] = data[jy*nx + ix];
+          else
+            valarr[iy] = DRMS_MISSING_FLOAT;
+          }
+        for (iy=0; iy<ny; iy++)
+          data[iy*nx + ix] = valarr[iy];
+        }
+      y0 -= yoff;
+      }
+    }
+  // update center location
+  ObsLoc->crpix1 = x0 + 1;
+  ObsLoc->crpix2 = y0 + 1;
+  return(0);
+  }
+
+// ----------------------------------------------------------------------
+
+int crop_image(DRMS_Array_t *arr, ObsInfo_t *ObsLoc)
+  {
+  int nx, ny, ix, iy, i, j, xoff, yoff;
+  double x0, y0;
+  double rsun = ObsLoc->rsun_obs/ObsLoc->cdelt1;
+  double scale, crop_limit2;
+  float *data;
+  if (!arr || !ObsLoc)
+    return(1);
+  data = arr->data;
+  nx = arr->axis[0];
+  ny = arr->axis[1];
+  x0 = ObsLoc->crpix1 - 1;
+  y0 = ObsLoc->crpix2 - 1;
+  scale = 1.0/rsun;
+  // crop_limit = 0.99975; // 1 - 1/4000, 1/2 HMI pixel.
+  crop_limit2 = 0.99950; // square of 1 - 1/4000, 1/2 HMI pixel.
+  for (iy=0; iy<ny; iy++)
+    for (ix=0; ix<nx; ix++)
+      {
+      double x, y, R2;
+      float *Ip = data + iy*nx + ix;
+      if (drms_ismissing_float(*Ip))
+        continue;
+      x = ((double)ix - x0) * scale; /* x,y in pixel coords */
+      y = ((double)iy - y0) * scale;
+      R2 = x*x + y*y;
+      if (R2 > crop_limit2)
+        *Ip = DRMS_MISSING_FLOAT;
+      }
+  return(0);
+  }
+
+// ----------------------------------------------------------------------
+
+#define CHECK(keyname) {if (status) {fprintf(stderr,"Keyword failure to find: %s, status=%d\n",keyname,status); *rstatus=status; return(NULL);}}
+
+ObsInfo_t *GetObsInfo(DRMS_Segment_t *seg, ObsInfo_t *pObsLoc, int *rstatus)
+  {
+  TIME t_prev;
+  DRMS_Record_t *rec;
+  TIME t_obs;
+  double dv;
+  ObsInfo_t *ObsLoc;
+  int status;
+
+  if (!seg || !(rec = seg->record))
+    { *rstatus = 1; return(NULL); }
+
+  ObsLoc = (pObsLoc ? pObsLoc : (ObsInfo_t *)malloc(sizeof(ObsInfo_t)));
+  if (!pObsLoc)
+    memset(ObsLoc, 0, sizeof(ObsInfo_t));
+
+  t_prev = ObsLoc->t_obs;
+  t_obs = drms_getkey_time(rec, "T_OBS", &status); CHECK("T_OBS");
+
+  if (t_obs <= 0.0)
+    { *rstatus = 2; return(NULL); }
+
+  if (t_obs != t_prev)
+    {
+    ObsLoc->crpix1 = drms_getkey_double(rec, "CRPIX1", &status); CHECK("CRPIX1");
+    ObsLoc->crpix2 = drms_getkey_double(rec, "CRPIX2", &status); CHECK("CRPIX2");
+    ObsLoc->crval1 = drms_getkey_double(rec, "CRVAL1", &status); CHECK("CRVAL1");
+    ObsLoc->crval2 = drms_getkey_double(rec, "CRVAL2", &status); CHECK("CRVAL2");
+    ObsLoc->cdelt1 = drms_getkey_double(rec, "CDELT1", &status); CHECK("CDELT1");
+    ObsLoc->cdelt2 = drms_getkey_double(rec, "CDELT2", &status); CHECK("CDELT1");
+    ObsLoc->crota2 = drms_getkey_double(rec, "CROTA2", &status); CHECK("CROTA2");
+    ObsLoc->sina = sin(ObsLoc->crota2*Deg2Rad);
+    ObsLoc->cosa = sqrt (1.0 - ObsLoc->sina*ObsLoc->sina);
+    ObsLoc->rsun_obs = drms_getkey_double(rec, "RSUN_OBS", &status);
+    if (status)
+      {
+      double dsun_obs = drms_getkey_double(rec, "DSUN_OBS", &status); CHECK("DSUN_OBS");
+      ObsLoc->rsun_obs = asin(696000000.0/dsun_obs)/arcsec2Rad;
+      }
+    ObsLoc->obs_vr = drms_getkey_double(rec, "OBS_VR", &status); CHECK("OBS_VR");
+    ObsLoc->obs_vw = drms_getkey_double(rec, "OBS_VW", &status); CHECK("OBS_VW");
+    ObsLoc->obs_vn = drms_getkey_double(rec, "OBS_VN", &status); CHECK("OBS_VN");
+    ObsLoc->obs_b0 = drms_getkey_double(rec, "CRLT_OBS", &status); CHECK("CRLT_OBS");
+    ObsLoc->t_obs = t_obs;
+    }
+  *rstatus = 0;
+  return(ObsLoc);
+  }
+
+// ----------------------------------------------------------------------
+
+// Generate explicit recordset list of closest good record to desired grid
+// First get vector of times and quality
+// Then if vector is not OK, quit.
+// then: make temp file to hold recordset list
+//       start with first time to define desired grid,
+//       make array of desired times.
+//       make empty array of recnums
+//       search vector for good images nearest desired times
+//       for each found time, write record query
+char *get_input_recset(DRMS_Env_t *drms_env, char *in, TIME cadence)
+  {
+  TIME epoch = (cmdparams_exists(&cmdparams, "epoch")) ? params_get_time(&cmdparams, "epoch") : 0;
+  DRMS_Array_t *data;
+  TIME t_start, t_stop, t_now, t_want, t_diff, this_t_diff;
+  int status;
+  int nrecs, irec;
+  int nslots, islot;
+  long long *recnums;
+  TIME *t_this, half = cadence/2.0;
+  double *drecnum, *dquality;
+  int quality;
+  long long recnum;
+  char keylist[DRMS_MAXQUERYLEN];
+  static char filename[100];
+  char *tmpdir;
+  FILE *tmpfile;
+  char *lbracket;
+  char seriesname[DRMS_MAXQUERYLEN];
+
+  sprintf(keylist, "T_OBS,QUALITY,recnum");
+  data = drms_record_getvector(drms_env, in, keylist, DRMS_TYPE_DOUBLE, 0, &status);
+  if (!data || status)
+        {
+        fprintf(stderr, "getkey_vector failed status=%d\n", status);
+        return(NULL);
+        }
+  nrecs = data->axis[1];
+  irec = 0;
+  t_this = (TIME *)data->data;
+  dquality = (double *)data->data + 1*nrecs;
+  drecnum = (double *)data->data + 2*nrecs;
+  if (epoch > 0.0)
+    {
+    int s0 = (t_this[0] - epoch)/cadence;
+    TIME t0 = s0*cadence + epoch;
+    t_start = (t0 < t_this[0] ? t0 + cadence : t0);
+    }
+  else
+    t_start = t_this[0];
+  t_stop = t_this[nrecs-1];
+  nslots = (t_stop - t_start + cadence/2)/cadence;
+  recnums = (long long *)malloc(nslots*sizeof(long long));
+  for (islot=0; islot<nslots; islot++)
+    recnums[islot] = 0;
+  islot = 0;
+  t_want = t_start;
+  t_diff = 1.0e9;
+  for (irec = 0; irec<nrecs; irec++)
+      {
+      t_now = t_this[irec];
+      quality = (int)dquality[irec] & 0xFFFFFFFF;
+      recnum = (long long)drecnum[irec];
+      this_t_diff = fabs(t_now - t_want);
+      if (quality < 0)
+        continue;
+      if (t_now <= (t_want-half))
+        continue;
+      while (t_now > (t_want+half))
+        {
+        islot++;
+        if (islot >= nslots)
+           break;
+        t_want = t_start + cadence * islot;
+        this_t_diff = fabs(t_now - t_want);
+        t_diff = 1.0e8;
+        }
+      if (this_t_diff <= t_diff)
+        recnums[islot] = recnum;
+      t_diff = fabs(t_now - t_want);
+      }
+  if (islot+1 < nslots)
+    nslots = islot+1;  // take what we got.
+  strcpy(seriesname, in);
+  lbracket = index(seriesname,'[');
+  if (lbracket) *lbracket = '\0';
+  tmpdir = getenv("TMPDIR");
+  if (!tmpdir) tmpdir = "/tmp";
+  sprintf(filename, "%s/hg_patchXXXXXX", tmpdir);
+  mkstemp(filename);
+  tmpfile = fopen(filename,"w");
+  for (islot=0; islot<nslots; islot++)
+    if (recnums[islot])
+      fprintf(tmpfile, "%s[:#%lld]\n", seriesname, recnums[islot]);
+  fclose(tmpfile);
+  free(recnums);
+  drms_free_array(data);
+  return(filename);
+  }
