3cc543316bb9fb6aa828da40b9fa2893b3995808
jim <jim>
2013-01-24 18:32:44 +0000

    *** empty log message ***

--- lev0/apps/Rules.mk
+++ lev0/apps/Rules.mk
@@ -8,11 +8,13 @@ LIBHKLEV0		:= $(d)/libhklev0.a
 test0_$(d)		:= $(addprefix $(d)/, test0)
 # wtest_$(d)		:= $(addprefix $(d)/, wtest)
 ingestlev0_$(d)		:= $(addprefix $(d)/, ingest_lev0 decode_dayfile)
+ingestlev0iris_$(d)	:= $(addprefix $(d)/, ingest_lev0_irisROT decode_dayfile)
 #ingestlev1_$(d)		:= $(addprefix $(d)/, build_lev1)
 #ingestlev1X_$(d)	:= $(addprefix $(d)/, build_lev1X)
 #ingestlev1_mgr_$(d)	:= $(addprefix $(d)/, build_lev1_mgr, build_lev1_mgr_fsn, build_lev1_mgrY)
 ingestlev1_mgr_$(d)	:= $(addprefix $(d)/, build_lev1_mgr)
 ingestlev0_obj_$(d)	:= $(addprefix $(d)/, imgdecode.o decode_hk.o  load_hk_config_files.o decode_hk_vcdu.o save_packet_to_dayfile.o write_hk_to_drms.o hmi_time_setting.o set_HMI_mech_values.o)
+ingestlev0iris_obj_$(d)	:= $(addprefix $(d)/, imgdecode_iris.o decode_hk.o  load_hk_config_files_iris.o decode_hk_vcdu.o save_packet_to_dayfile.o write_hk_to_drms_iris.o iris_time_setting.o)
 xingestlev0_obj_$(d)	:= $(addprefix $(d)/, imgdecode.o decode_hk.o  load_hk_config_files.o decode_hk_vcdu.o save_packet_to_dayfile.o write_hk_to_drms.o hmi_time_setting.o set_HMI_mech_values.o)
 #yingestlev0_obj_$(d)	:= $(addprefix $(d)/, imgdecode.o decode_hk.o  load_hk_config_files.o decode_hk_vcdu.o save_packet_to_dayfile.o write_hk_to_drms_test.o hmi_time_setting_test.o set_HMI_mech_values.o)
 #xingestlev0_$(d)	:= $(addprefix $(d)/, xingest_lev0 decode_dayfile)
@@ -29,7 +31,7 @@ printtime_$(d)		:= $(addprefix $(d)/, printtime)
 #SUMEXE_$(d)	:= $(ingestlev0_$(d)) $(xingestlev0_$(d)) $(ingestlev1_$(d)) $(ingestlev1_mgr_$(d)) $(test0_$(d)) $(wtest_$(d))
 #SUMEXE_$(d)	:= $(ingestlev0_$(d)) $(xingestlev0_$(d)) $(ingestlev1_$(d)) $(ingestlev1_mgr_$(d)) $(ingestlev1X_$(d)) $(wtest_$(d))
 #SUMEXE_$(d)     := $(ingestlev0_$(d)) $(xingestlev0_$(d)) $(ingestlev1_$(d)) $(ingestlev1_mgr_$(d)) $(wtest_$(d))
-SUMEXE_$(d)	:= $(ingestlev0_$(d)) $(xingestlev0_$(d)) $(yingestlev0_$(d)) $(ingestlev1_$(d)) $(ingestlev1_mgr_$(d)) $(printtime_$(d))
+SUMEXE_$(d)	:= $(ingestlev0_$(d)) $(ingestlev0iris_$(d)) $(xingestlev0_$(d)) $(yingestlev0_$(d)) $(ingestlev1_$(d)) $(ingestlev1_mgr_$(d)) $(printtime_$(d))
 #SUMEXE_$(d)	:= $(ingestlev0_$(d)) $(ingestlev1_$(d)) $(ingestlev1_mgr_$(d)) $(printtime_$(d))
 CEXE_$(d)       := $(addprefix $(d)/, fix_hmi_config_file_date)
 CEXE		:= $(CEXE) $(CEXE_$(d))
@@ -65,7 +67,7 @@ MODEXEDROBJ	:= $(MODEXEDROBJ) $(MODEXEDR_$(d):%=%.o)
 
 ALLEXE_$(d)	:= $(MODEXE_$(d)) $(MODEXEDR_$(d)) $(MODEXE_USEF_$(d)) $(SUMEXE_$(d)) $(CEXE_$(d))
 #OBJ_$(d)	:= $(ALLEXE_$(d):%=%.o) 
-OBJ_$(d)	:= $(ALLEXE_$(d):%=%.o) $(TESTEXE_USEF_$(d):%=%.o) $(ingestlev0_obj_$(d))
+OBJ_$(d)	:= $(ALLEXE_$(d):%=%.o) $(TESTEXE_USEF_$(d):%=%.o) $(ingestlev0_obj_$(d)) $(ingestlev0iris_obj_$(d))
 DEP_$(d)	:= $(OBJ_$(d):%=%.d)
 CLEAN		:= $(CLEAN) \
 		   $(OBJ_$(d)) \
@@ -81,6 +83,7 @@ TGT_LIB		:= $(TGT_LIB) $(LIBHKLEV0)
 S_$(d)		:= $(notdir $(ALLEXE_$(d)) $(TESTEXE_USEF_$(d)) $(MODEXE_SOCK_$(d)))
 
 $(ingestlev0_$(d)):	$(ingestlev0_obj_$(d))
+$(ingestlev0iris_$(d)):	$(ingestlev0iris_obj_$(d))
 $(xingestlev0_$(d)):	$(xingestlev0_obj_$(d))
 #$(yingestlev0_$(d)):	$(yingestlev0_obj_$(d))
 
@@ -110,6 +113,7 @@ $(LIBHKLEV0):		$(LIBHKLEV0_OBJ)
 			$(SLLIB)
 
 $(ingestlev0_$(d)):	LL_TGT := $(LL_TGT) -lpng
+$(ingestlev0iris_$(d)):	LL_TGT := $(LL_TGT) -lpng
 $(xingestlev0_$(d)):	LL_TGT := $(LL_TGT) -lpng
 #$(yingestlev0_$(d)):	LL_TGT := $(LL_TGT) -lpng
 $(ingestlev1_$(d)):	LL_TGT := $(LL_TGT)
