3a12436e2b818c3adb5c436d25cdaf7ff6fc634a
rick <rick>
2013-04-08 22:27:51 +0000

    modified for JSOC release 8.0

--- rings/apps/Rules.mk
+++ rings/apps/Rules.mk
@@ -6,9 +6,9 @@ d		:= $(dir)
 # Local variables
 # NOTE: Add the base of the module's filename below (next to mymod)
 RDVINV_$(d)	:= 
-#ifeq ($(JSOC_MACHINE), linux_x86_64)
+ifeq ($(JSOC_MACHINE), linux_x86_64)
 RDVINV_$(d)	:= $(addprefix $(d)/, rdvinv)
-#endif
+endif
 
 ifeq ($(COMPILER), icc)
   MODEXE_$(d)	:= $(addprefix $(d)/, datavg maicalc maproj mtrack pspec3 rdcover rdfitc xtrackd) 
@@ -40,12 +40,21 @@ S_$(d)		:= $(notdir $(EXE_$(d)) $(MODEXE_SOCK_$(d)))
 
 # Local rules
 $(OBJ_$(d)):		$(SRCDIR)/$(d)/Rules.mk
-$(OBJ_$(d)):		CF_TGT := $(CF_TGT) -DCDIR="\"$(SRCDIR)/$(d)\"" $(FFTWH)
+$(OBJ_$(d)):		CF_TGT := $(CF_TGT) -I/home/jsoc/include -DCDIR="\"$(SRCDIR)/$(d)\""
 
-$(MODEXE_$(d)) $(MODEXE_SOCK_$(d)):	LL_TGT := $(LL_TGT) -L$(FFTW_LIBS) -lfftw3 -lfftw3f
+ifeq ($(JSOC_MACHINE), linux_ia32)
+  FFTW_$(d) = /home/jsoc/lib/linux-ia32
+endif
+ifeq ($(JSOC_MACHINE), linux_x86_64)
+  FFTW_$(d) = /home/jsoc/lib/linux-x86_64
+endif
 
+$(MODEXE_$(d)) $(MODEXE_SOCK_$(d)):	LL_TGT := $(LL_TGT) -L$(FFTW_$(d)) -lfftw3 -lfftw3f
+
+ifeq ($(JSOC_MACHINE), linux_x86_64)
 $(RDVINV_$(d)):				$(OBJ_OLAXY_$(d))
 $(RDVINV_$(d)):				LL_TGT := $(LL_TGT) -lmkl_em64t
+endif
 
 # Shortcuts
 .PHONY:	$(S_$(d))
