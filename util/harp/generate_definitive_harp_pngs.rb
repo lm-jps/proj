a7ce47f9399ed1e71e82c0009b875d247217f1dc
mbobra <mbobra>
2013-06-07 20:12:37 +0000

    Implemented T_HORIZN

--- util/harp/generate_definitive_harp_pngs.rb
+++ util/harp/generate_definitive_harp_pngs.rb
@@ -8,7 +8,7 @@
 # Machine  Use only on solar3 (the only machine that supports both ruby and MATLAB). 
 #
 #          This script runs in two modes, forward and backward. 
-#          Forward mode -- this is the mode to run in cron every hour. 
+#          Forward mode -- this is the mode to run every day after the tracker run. 
 #          It creates a definitive HARP .png file at T_REC = (now - 27 days). 
 #          The forward mode only takes the arguments [TYPE] and [DIR] (see below).
 #
@@ -42,18 +42,37 @@ end
 
 # Run in backward mode.
 if type == 'backward'
-
 `/home/jsoc/cvs/Development/JSOC/proj/mag/harp/scripts/track_hmi_harp_movie_driver.sh -f 'hmi.Marmask_720s[#{tstart}-#{tstop}@1h]' hmi.Mharp_720s #{dir}`
-
 end
+
 
 # Run in forward mode.
 if type == 'forward'
 
-t1  = Time.now.utc
-t2  = t1 - (60 * 60 * 24 * 27) #27 days in seconds
-t3  = t2.strftime('%Y')+'.'+t2.strftime('%m')+'.'+t2.strftime('%d')+'_'+t2.strftime('%H')+':00:00_TAI'
+# Determine the range of times for which to compute images
+horizon = `show_info -q key=T_HORIZN ds=hmi.Mharp_log_720s"[]" n= -2`.split
 
-`/home/jsoc/cvs/Development/JSOC/proj/mag/harp/scripts/track_hmi_harp_movie_driver.sh -f 'hmi.Marmask_720s[#{t3}]' hmi.Mharp_720s #{dir}`
+if horizon[0] == horizon[1] 
+ $stderr.puts "There have been no updates to hmi.Marmask_720s and hmi.Mharp_720s data since this script was last run."
+ exit
+end
+
+# TIME A
+# Determine the last T_REC at which hmi.Marmask_720s and hmi.Mharp_720s data are available 
+# This is 12 minutes before the result of show_info 'ds=hmi.Mharp_log_720s[$]' key=T_HORIZN
+t1a      = DateTime.strptime(horizon[1], '%Y.%m.%d_%H:%M:%S_TAI').strftime('%s').to_i - (60 * 12)
+t2a      = Time.at(t1a).utc
+t3a      = t2a.strftime('%Y')+'.'+t2a.strftime('%m')+'.'+t2a.strftime('%d')+'_'+t2a.strftime('%H')+':00:00_TAI'
+
+# TIME B
+# Determine the first T_REC at which hmi.Marmask_720s and hmi.Mharp_720s data are available 
+# This is the first result of show_info -q key=T_REC ds=hmi.Mharp_log_720s"[]" n= -2.
+# However, since this may not be on the hour (i.e., at _HH:00:00_TAI), go back one hour.
+t1b      = DateTime.strptime(horizon[0], '%Y.%m.%d_%H:%M:%S_TAI').strftime('%s').to_i - (60 * 60)
+t2b      = Time.at(t1b).utc
+t3b      = t2b.strftime('%Y')+'.'+t2b.strftime('%m')+'.'+t2b.strftime('%d')+'_'+t2b.strftime('%H')+':00:00_TAI'
+
+# Generate once-per hour images between TIME B and TIME A:
+`/home/jsoc/cvs/Development/JSOC/proj/mag/harp/scripts/track_hmi_harp_movie_driver.sh -f 'hmi.Marmask_720s[#{t3b}-#{t3a}@1h]' hmi.Mharp_720s #{dir}`
 
 end
