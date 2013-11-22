341ab56f9411baf91effc48ab2a32a8645d28d55
cvs-fast-export <cvs-fast-export>
2013-11-22 19:29:40 +0000

    Synthetic commit for incomplete tag NetDRMS_Ver_8-2

--- mag/ambig/apps/pot_field.f90
+++
@@ -1,6 +0,0 @@
-!***********************************************************************
-! Module containing the arrays containing the potential field.         *
-!***********************************************************************
-module pot_field
-   real,dimension(:,:),allocatable :: Bpix,Bpiy,dBpzdz
-end module pot_field
