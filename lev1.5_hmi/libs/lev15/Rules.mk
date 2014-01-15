441cf68d707a34b4397089a498ac9d6f06f988a2
jsoc <jsoc>
2014-01-15 18:10:34 +0000

    Fix check-in - rule to make test files accidentally checked-in.

--- lev1.5_hmi/libs/lev15/Rules.mk
+++ lev1.5_hmi/libs/lev15/Rules.mk
@@ -6,7 +6,11 @@ d		:= $(dir)
 LIBLEV15	:= $(d)/liblev15.a
 
 
-OBJ_$(d)	:= $(addprefix $(d)/, interpol_code.o polcal.o Dopplergram.o Dopplergram2.o fstats.o fstats2.o dstats.o dstats2.o Dopplergram_test.o Dopplergram_test2.o Dopplergram_duvall.o Dopplergram_Jesper.o Dopplergram_largercrop.o)
+# OBJ_$(d)	:= $(addprefix $(d)/, interpol_code.o polcal.o Dopplergram.o Dopplergram2.o fstats.o fstats2.o dstats.o dstats2.o Dopplergram_test.o Dopplergram_test2.o Dopplergram_duvall.o Dopplergram_Jesper.o Dopplergram_largercrop.o)
+
+# Fix broken build?
+#  -ART 2014/01/15
+OBJ_$(d)	:= $(addprefix $(d)/, interpol_code.o polcal.o Dopplergram.o Dopplergram2.o fstats.o fstats2.o dstats.o dstats2.o Dopplergram_largercrop.o)
 
 DEP_$(d)	:= $(OBJ_$(d):%=%.d)
 
