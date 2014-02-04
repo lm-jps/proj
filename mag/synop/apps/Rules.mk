0e687c23d76df24f14a6b59ae11b53dc98513f62
yliu <yliu>
2014-02-04 00:46:58 +0000

    New module to produce both Br and Blos synoptic charts

--- mag/synop/apps/Rules.mk
+++ mag/synop/apps/Rules.mk
@@ -9,7 +9,7 @@ d		:= $(dir)
 EXTRADEPS_$(d)		:= $(addprefix $(d)/, )
 
 # NOTE: Add the base of the module's filename below (next to mymod)
-MODEXE_$(d)	:= $(addprefix $(d)/, dailysynframe dailysynframe_nrt hmisynoptic mdidailysynframe mdisynop)
+MODEXE_$(d)	:= $(addprefix $(d)/, dailysynframe dailysynframe_nrt hmisynoptic mdidailysynframe mdisynop brblossynoptic)
 MODEXE		:= $(MODEXE) $(MODEXE_$(d))
 
 MODEXE_SOCK_$(d):= $(MODEXE_$(d):%=%_sock)
