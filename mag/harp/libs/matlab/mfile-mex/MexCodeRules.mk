ff3c5bb87c6fb26c3472b299970f77760f47e178
arta <arta>
2014-02-10 22:49:42 +0000

    Get rid of debug print statement in make files. Use .PHONY for a fake file name.

--- mag/harp/libs/matlab/mfile-mex/MexCodeRules.mk
+++ mag/harp/libs/matlab/mfile-mex/MexCodeRules.mk
@@ -21,11 +21,11 @@ COMPILE.cmex = $(CMEX) -c $(CMEXFLAGS) $(CMEXPPFLAGS)
 # PROBLEM - you must use -outdir $(OUTDIR)/$(CSDIR) otherwise
 # the compilation of .o from .c will result in files being placed 
 # in /home/arta/jsoctrees/JSOC/_linux_x86_64. For some reason,
-# mex this this is the output/working directory, even though
+# mex thinks this is the output/working directory, even though
 # make thinks the output/working directory is 
-# /home/arta/jsoctrees/JSOC/proj/mag/harp/libs/matlab/mfile-mex/standalone.
+# /home/arta/jsoctrees/JSOC/_linux_x86_64/proj/mag/harp/libs/matlab/mfile-mex/standalone.
 # But setting the -outdir argument 
-# causes problems with where mex puts the .mexa64 files.For some
+# causes problems with where mex puts the .mexa64 files. For some
 # reason mex pre-pends the outdir to the FULL PATH to the mexa64 files.
 # It doesn't do that with the .o files. I think what mex is doing 
 # is that is ALWAYS pre-pends its notion of OUTDIR to the target files.
@@ -45,13 +45,28 @@ COMPILE.cmex = $(CMEX) -c $(CMEXFLAGS) $(CMEXPPFLAGS)
 #	$(LINK.cmex) $(OUTPUT_OPTION) $<
 
 # This works, despite its inelegance!
-
+#
+# The files %.$(MEXEXT) do not ever exist. They are needed because mex needs them. mex prepends what it considers the output
+# directory to these targets. If instead the rule that includes the mex recipe had $(OUTDIR)/$(CSDIR)/%.$(MEXEXT) as the target
+# (which is what appears as a prerequisite in another target), then mex would attempt to create $(OUTDIR)/$(CSDIR)/$(OUTDIR)/$(CSDIR)/%.$(MEXEXT). 
+# When mex creates the .o from the .c files, it also prepends the output directory to the .o files. So the rules for the %.$(MEXEXT) targets below 
+# work just fine, except that the files %.$(MEXEXT) never exist! To cope with this, I created a .PHONY rule for %.$(MEXEXT). This way,
+# whenever there is an attempt to make $(OUTDIR)/$(CSDIR)/%.$(MEXEXT), this will result in an attempt to make %.$(MEXEXT), which will
+# ALWAYS result in the build of the .o files from the .c files, and it will create the *.mexa64 files (even if the *.mexa64 files
+# somehow ended up existing in /home/arta/jsoctrees/JSOC/_linux_x86_64, which is the place where make is looking for them).
+#
+# Apparently, every single target must have a recipe - the rule for $(OUTDIR)/$(CSDIR)/%.$(MEXEXT) is no exception. If you don't provide
+# a recipe in any target, then make does not know how to build the target, and when it comes time to build the target (because it
+# appears as a prerequisite in another rule), make will fail. But to make $(OUTDIR)/$(CSDIR)/%.$(MEXEXT) from %.$(MEXEXT), make doesn't
+# have to do anything because %.$(MEXEXT) doesn't really exist. So, instead of providing a recipe that does something, I just put a 
+# semicolon at the end of the prerequsite for $(OUTDIR)/$(CSDIR)/%.$(MEXEXT) - an "empty" recipe that does nothing. If you
+# do not provide a recipe at all, then thinks there is no rule for $(OUTDIR)/$(CSDIR)/%.$(MEXEXT) at all, as mentioned already.
+#
+.PHONY: %.$(MEXEXT)
 %.$(MEXEXT): %.c Doc/%_docstring.h
 	$(LINK.cmex) $(OUTPUT_OPTION) $<
-
-# For some reason, make needs a recipe in this rule. I used to have an echo command here. Now I have whitespace and a semicolon, which does nothing.
-# But this is necessary to get this working - I have no idea why. If you don't have a recipe, then the dependency part is ignored...for some
-# reason.
+# LHS - full path /home/arta/jsoctrees/JSOC/_linux_x86_64/proj/mag/harp/libs/matlab/mfile-mex/standalone/*.mexa64
+# RHS - base name *.mexa64 (these files do not actually exist - they would be /home/arta/jsoctrees/JSOC/_linux_x86_64/*.mexa64)
 $(OUTDIR)/$(CSDIR)/%.$(MEXEXT): %.$(MEXEXT) ;
 # end this works
 
