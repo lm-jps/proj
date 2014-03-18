a6eb8c1ba7666dbf0fc5b61e7d291c81bc48c33f
arta <arta>
2014-03-18 18:51:39 +0000

    Update for SUMS db on hmidb3. Eliminate redundant report.

--- util/scripts/gensureports.pl
+++ util/scripts/gensureports.pl
@@ -13,27 +13,16 @@ my($datasec);
 
 
 # All Data
-$cmd = kScrPath . "/sumstapestat.pl jsoc_sums hmidb 5434 production agg group all > " . kWdPath . "/" . kTmpFile;
+$cmd = kScrPath . "/sumstapestat.pl jsoc_sums hmidb3 5434 production agg group all > " . kWdPath . "/" . kTmpFile;
 `$cmd`;
 
 if (open(TMPFILE, "<" . kWdPath . "/" . kTmpFile) && open(REPFILE, ">" . kWdPath . "/sureportA.txt"))
 {
-   FilterData(\*TMPFILE, \*REPFILE);
-
-   close(TMPFILE);
-   close(REPFILE);
-}
-
-# AP Data
-$cmd = kScrPath . "/sumstapestat.pl jsoc_sums hmidb 5434 production agg group ap > " . kWdPath . "/" . kTmpFile;
-`$cmd`;
-
-if (open(TMPFILE, "<" . kWdPath . "/" . kTmpFile) && open(REPFILE, ">" . kWdPath . "/sureportB.txt"))
-{
-   FilterData(\*TMPFILE, \*REPFILE);
-
-   close(TMPFILE);
-   close(REPFILE);
+    print REPFILE "Report generated on " . localtime() . "\n";
+    FilterData(\*TMPFILE, \*REPFILE);
+    
+    close(TMPFILE);
+    close(REPFILE);
 }
 
 exit(0);
@@ -41,30 +30,30 @@ exit(0);
 # subroutines
 sub FilterData
 {
-   my($tmpfile) = $_[0];
-   my($repfile) = $_[1];
-   my($datas) = kDataSec;
-   my($ends) = kEndSec;
- 
-   $datasec = 0;
-   while (defined($line = <$tmpfile>))
-   {
-      chomp($line);
-
-      if ($line =~ /$datas/)
-      {
-         $datasec = 1;
-         next;
-      }
-      elsif ($line =~ /$ends/)
-      {
-         $datasec = 0;
-         last;
-      }
-
-      if ($datasec)
-      {
-         print $repfile "$line\n";
-      }
-   }     
+    my($tmpfile) = $_[0];
+    my($repfile) = $_[1];
+    my($datas) = kDataSec;
+    my($ends) = kEndSec;
+    
+    $datasec = 0;
+    while (defined($line = <$tmpfile>))
+    {
+        chomp($line);
+        
+        if ($line =~ /$datas/)
+        {
+            $datasec = 1;
+            next;
+        }
+        elsif ($line =~ /$ends/)
+        {
+            $datasec = 0;
+            last;
+        }
+        
+        if ($datasec)
+        {
+            print $repfile "$line\n";
+        }
+    }     
 }
