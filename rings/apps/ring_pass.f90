a41eacf24ce6263232c9dc437049e325799cbdc5
cvs-fast-export <cvs-fast-export>
2014-06-17 21:47:13 +0000

    Synthetic commit for incomplete tag globalhs_version_4

--- rings/apps/ring_pass.f90
+++
@@ -1,7 +0,0 @@
-   module ring_pass
-     implicit none
-     integer, parameter :: sle = 4
-     integer, parameter :: dle = 8
-     real(kind=dle) :: dk, dnu
-     integer :: istk, iendk, ni, nj, npj, v, ntot
-   end module ring_pass
