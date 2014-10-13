401611493f10c079aad30a331db60a21f34174c9
jps <jps>
2014-10-13 22:34:32 +0000

    Add date_beg and end options, use sprint_time_ISO for dates.

--- util/apps/pfss_backup.c
+++ util/apps/pfss_backup.c
@@ -9,6 +9,8 @@ ModuleArgs_t module_args[] = {
   {ARG_STRING, "ds", "lm_jps.pfss", "output data series"},
   {ARG_STRING, "ser_beg", NOTSPECIFIED, "serial number to begin"},
   {ARG_STRING, "ser_end", NOTSPECIFIED, "serial number to end (not saved)"},
+  {ARG_STRING, "date_beg", NOTSPECIFIED, "date to begin"},
+  {ARG_STRING, "date_end", NOTSPECIFIED, "date to end (not saved)"},
   {ARG_STRING, "spath", "/archive/pfss/kitrun48/surffield-serial", ""},
   {ARG_STRING, "bpath", "/archive/pfss/kitrun48/Bfield-serial", ""},
   {ARG_FLAG, "h", "0", "Print usage message and quit"},
@@ -18,6 +20,14 @@ ModuleArgs_t module_args[] = {
 
 char *module_name = "pfss_backup";
 int verbose = 0;
+
+void sprint_time_ISO (char *tstring, TIME t) 	 
+{ 	 
+  sprint_at(tstring,t); 	 
+  tstring[4] = tstring[7] = '-'; 	 
+  tstring[10] = 'T'; 	 
+  tstring[19] = '\0'; 	 
+}
 
 int nice_intro(int help)
 {
@@ -29,7 +39,9 @@ int nice_intro(int help)
         "  -v: verbose\n"
         "ds=<output series name> default is lm_jps.pfss\n"
         "ser_beg=<first serial number to backup>\n"
-        "ser_end=<serial number to stop> (not backup)\n");
+        "ser_end=<serial number to stop> (not backup)\n"
+        "date_beg=<first date to backup>\n"
+        "date_end=<date to stop> (not backup)\n");
     return(1);
   }
   return(0);
@@ -37,10 +49,11 @@ int nice_intro(int help)
 
 int DoIt ()
 {
-  char *ds, *spath, *bpath, sname[512], bname[512];
+  char *date_beg, *date_end, *ds, *spath, *bpath;
+  char sname[512], bname[512], now_str[64];
   int ser_beg, ser_end, ser_num, status=0;
-  int s_ok, b_ok; /* yr, mo, da, hr, mn; */
-  time_t trec;
+  int s_ok, b_ok, sp_ok, bp_ok; /* yr, mo, da, hr, mn; */
+  time_t trec, tnow;
   DRMS_Record_t *outrec;
   DRMS_Segment_t *outseg;
   struct tm tm_rec;
@@ -52,31 +65,63 @@ int DoIt ()
   ds = strdup(cmdparams_get_str(&cmdparams, "ds", NULL));
   spath = strdup(cmdparams_get_str(&cmdparams, "spath", NULL));
   bpath = strdup(cmdparams_get_str(&cmdparams, "bpath", NULL));
+  if (ser_beg < 0) {
+    time_t tr_b, tr_e;
+    date_beg = strdup(cmdparams_get_str(&cmdparams, "date_beg", NULL));
+    tr_b = sscan_time(date_beg) - UNIX_EPOCH + 218;
+    ser_beg = (tr_b - 836179440)/21600;
+    date_end = strdup(cmdparams_get_str(&cmdparams, "date_end", NULL));
+    tr_e = sscan_time(date_end) - UNIX_EPOCH + 218;
+    ser_end = (tr_e - 836179440)/21600;
+    if (verbose) {
+      printf("trec: %ld %ld,ser_beg, end: %d, %d\n",tr_b,tr_e,ser_beg,ser_end);
+    }
+  }
+  if (stat(spath, &sbuf)) {
+    sp_ok = 0;
+    fprintf(stderr, "Can not stat %s\n", spath);
+  } else sp_ok = 1;
+  if (stat(bpath, &sbuf)) {
+    bp_ok = 0;
+    fprintf(stderr, "Can not stat %s\n", bpath);
+  } else bp_ok = 1;
 
   for (ser_num = ser_beg; ser_num < ser_end; ser_num++) {
-    trec = 21600*ser_num + 836179440;
+    trec = 21600*ser_num + 836179440 - 8;
+    if (verbose) {
+      printf("trec: %ld,ser_beg, end: %d, %d\n", trec, ser_beg, ser_end);
+    }
     gmtime_r(&trec, &tm_rec);
     snprintf(sname, 512, "%s/kitrun048_%5.5d.sav", spath, ser_num);
     snprintf(bname, 512, "%s/Bfield_%5.5d.sav", bpath, ser_num);
     outrec = drms_create_record(drms_env, ds, DRMS_PERMANENT, &status);
     if (status) DIE("Can't create output record");
-    drms_setkey_time(outrec, "model_date", (double) (trec + UNIX_EPOCH));
+//  drms_setkey_time(outrec, "model_date", (double) (trec + UNIX_EPOCH));
+    sprint_time_ISO(now_str, trec + UNIX_EPOCH);
+    drms_setkey_string(outrec, "model_date", now_str);
+    if (verbose) printf("model_datei: %s\n", now_str);
     drms_setkey_int(outrec, "serial", ser_num);
     if (stat(sname, &sbuf)) {
       s_ok = 0;
-      fprintf(stderr, "Can not stat %s\n", sname);
+      if (sp_ok) fprintf(stderr, "Can not stat %s\n", sname);
     } else {
       s_ok = 1;
-      drms_setkey_time(outrec,"s_calc_date",(double)(sbuf.st_mtime+UNIX_EPOCH));
+//    drms_setkey_time(outrec,"s_calc_date",(double)(sbuf.st_mtime+UNIX_EPOCH));
+      sprint_time_ISO(now_str, sbuf.st_mtime + UNIX_EPOCH);
+      drms_setkey_string(outrec, "s_calc_date", now_str);
     }
     if (stat(bname, &sbuf)) {
       b_ok = 0;
-      fprintf(stderr, "Can not stat %s\n", bname);
+      if (bp_ok) fprintf(stderr, "Can not stat %s\n", bname);
     } else {
       b_ok = 1;
-      drms_setkey_time(outrec,"b_calc_date",(double)(sbuf.st_mtime+UNIX_EPOCH));
+//    drms_setkey_time(outrec,"b_calc_date",(double)(sbuf.st_mtime+UNIX_EPOCH));
+      sprint_time_ISO(now_str, sbuf.st_mtime + UNIX_EPOCH);
+      drms_setkey_string(outrec, "b_calc_date", now_str);
     }
-    drms_setkey_time(outrec, "backup_date", CURRENT_SYSTEM_TIME);
+    tnow = CURRENT_SYSTEM_TIME;
+    drms_setkey_time(outrec, "backup_date", tnow);
+    sprint_time_ISO(now_str, tnow);
     if (s_ok) {
       outseg = drms_segment_lookup(outrec, "surffield");
       if(drms_segment_write_from_file(outseg, sname)) {
@@ -89,7 +134,7 @@ int DoIt ()
         fprintf(stderr, "Can not write B field segment for %s.\n", bname);
       }
     }
-    if (s_ok && b_ok) status = drms_close_record(outrec, DRMS_INSERT_RECORD);
+    if (s_ok || b_ok) status = drms_close_record(outrec, DRMS_INSERT_RECORD);
     else status = drms_close_record(outrec, DRMS_FREE_RECORD);
     if(status) fprintf(stderr, "Error closing rec for ser #: %d.\n", ser_num);
   }
