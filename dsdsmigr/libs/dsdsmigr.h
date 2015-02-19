40cc178fc57d9e113e5719631c036cf409c3d829
cvs-fast-export <cvs-fast-export>
2015-02-19 15:12:50 +0000

    Synthetic commit for incomplete tag NetDRMS_Ver_8-7

--- dsdsmigr/libs/dsdsmigr.h
+++
@@ -1,7 +0,0 @@
-#ifndef __DSDSMIGR_H
-#define __DSDSMIGR_H
-
-int soho_ephemeris (TIME obs_time, double *r, double *b, double *l,
-                    double *vr, double *vn, double *vw, TIME *table_mod_time);
-
-#endif
