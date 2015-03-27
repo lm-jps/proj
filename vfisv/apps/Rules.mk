b7954e3cab04856a1ce19131e855b06f08c9adcf
arta <arta>
2015-03-27 16:36:19 +0000

    Instead of enforcing inter-Fortran-module dependencies with make rules, simply build the Fortran modules in the correct order, so that dependencies are built before the modules that depened on them.

--- vfisv/apps/Rules.mk
+++ vfisv/apps/Rules.mk
@@ -13,22 +13,75 @@ VFISV_$(d)	:= $(addprefix $(d)/, vfisv vfisv_harp vfisv_fd10_old vfisv_fd10_harp
 
 VFISV_COBJ_$(d)	:= $(VFISV_$(d):%=%.o)
 
-# VFISV_$(d) depends on Fortran code
-VFISV_FOBJ_$(d)	:= $(addprefix $(d)/, $(notdir $(patsubst %.f,%.o,$(wildcard $(SRCDIR)/$(d)/*.f))))
-VFISV_FOBJ_$(d)	:= $(VFISV_FOBJ_$(d)) $(addprefix $(d)/, $(notdir $(patsubst %.f90,%.o,$(wildcard $(SRCDIR)/$(d)/*.f90))))
+# VFISV_$(d) depends on Fortran code. 
+# Here's the skinny on Fortran 90 modules. If a Fortran 90 file 
+# contains a MODULE statement, then a .mod file will be produced by the compiler. To use a module file
+# a Fortran 90 file uses the USE statement. However, when you COMPILE
+# a Fortran 90 file with a USE statement in it, there must exist a .mod file for the module
+# in the USE statement. If a Fortran 90 file has 'USE FILT_PARAM', then AT COMPILE TIME 
+# filt_param.mod must exist. If you want to link a bunch of Fortran 90 files together, then this implies 
+# that you must compile the Fortran 90 files in a specific order - you must compile dependent files 
+# before you compile the files that require the dependent files. 
+#
+# This dependency chain was previous enforced with make rules between the various .o files
+# generated from the .f90 files. However, this was causing the timestamps of dependent files
+# to be newer than the timestamps of the files that depend on them. So make vfisv would always
+# build something every time make ran, even though this was unnecessary.  Instead, we now specify the order of 
+# compilation of the f90 files by listing them in the order in which they should be built.
 
-# Actually, we don't need dmrs.f90 for now
-# EXCLUDE_$(d)	:= $(d)/dmrs.o
-# VFISV_FOBJ_$(d) := $(filter-out $(EXCLUDE_$(d)),$(VFISV_FOBJ_$(d)))
+# Compile in this order (this is ONE order that works - there are others). See CVS version
+# 1.11 for the list of actual dependencies between these Fortran object files.
+#   cons_param.o
+#   line_param.o
+#   filt_param.o
+#   change_var.o
+#   filt_init.o
+#   inv_param.o
+#   inv_init.o
+#   lim_init.o
+#   wfa_guess.o
+#   ran_mod.o
+#   line_init.o
+#   svbksb.o
+#   voigt_data.o
+#   voigt_init.o
+#   wave_init.o
+#   voigt.o
+#   voigt_taylor.o
+#   forward.o
+#   free_init.o
+#   free_memory.o
+#   inv_utils.o
+#   invert.o
+
+# Just list all the Fortran files since we have to build them in a specific order (see note below).
+# They will be built in the order they appear in this list.
+VFISV_FOBJ_$(d) := $(addprefix $(d)/, cons_param.o \
+    line_param.o \
+    filt_param.o \
+    change_var.o \
+    filt_init.o \
+    inv_param.o \
+    inv_init.o \
+    lim_init.o \
+    wfa_guess.o \
+    ran_mod.o \
+    line_init.o \
+    svbksb.o \
+    voigt_data.o \
+    voigt_init.o \
+    wave_init.o \
+    voigt.o \
+    voigt_taylor.o \
+    forward.o \
+    free_init.o \
+    free_memory.o \
+    inv_utils.o \
+    invert.o)
 
 MODEXE_USEF_$(d):= $(VFISV_$(d))
 MODEXE_USEF	:= $(MODEXE_USEF) $(MODEXE_USEF_$(d))
 
-# Not available as a sock module - uses drms_server_end_transaction()
-# MODEXE_USEF_SOCK_$(d)	:= $(addsuffix _sock, $(MODEXE_USEF_$(d)))
-# MODEXE_USEF_SOCK	:= $(MODEXE_USEF_SOCK) $(MODEXE_USEF_SOCK_$(d))
-
-#EXE_$(d)        := $(MODEXE_USEF_$(d)) $(MODEXE_USEF_SOCK_$(d))
 EXE_$(d)        := $(MODEXE_USEF_$(d))
 OBJ_$(d)	:= $(VFISV_COBJ_$(d)) $(VFISV_FOBJ_$(d))
 DEP_$(d)	:= $(OBJ_$(d):%=%.d)
@@ -43,7 +96,7 @@ S_$(d)		:= $(notdir $(EXE_$(d)))
 
 # Local rules
 $(OBJ_$(d)):		$(SRCDIR)/$(d)/Rules.mk
-$(OBJ_$(d)):		CF_TGT := $(CF_TGT) -I$(MPI_INCS) -DCDIR="\"$(SRCDIR)/$(d)\""
+$(OBJ_$(d)):		CF_TGT := $(CF_TGT) -I$(MPI_INCS)
 $(EXE_$(d)):		LL_TGT := $(LL_TGT) -lmkl_em64t
 
 # Use a non-standard Fortran compiler for LINKING the modules of this make file.
@@ -51,37 +104,6 @@ $(EXE_$(d)):		FCOMPILER := $(MPIFCOMPILER)
 # The line above should NOT affect the compilation of the object files, but it does!
 $(VFISV_FOBJ_$(d)):	FCOMPILER := ifort
 $(VFISV_COBJ_$(d)):	ICC_CMPLR := $(MPICOMPILER)
-
-# Here's the skinny on Fortran 90 modules. If a Fortran 90 file 
-# contains a MODULE statement, then a .mod file will be produced by the compiler. To use a module file
-# a Fortran 90 file can use the USE statement. However, when you COMPILE
-# a Fortran 90 file with a USE statement in it, there must exist a .mod file for that USE statement.
-# If a Fortran 90 file has USE FILT_PARAM, then AT COMPILE TIME filt_param.mod must exist. If you want to 
-# link a bunch of Fortran 90 files together, then this implies that you must compile the Fortran 90
-# files in a specific order - you must compile dependent files before you compile the files that require
-# the dependent files. So we have to specify the order of compilation of the f90 files (by specifying
-# the dependency relationships between them.
-$(d)/change_var.o: $(d)/cons_param.o  $(d)/filt_param.o
-$(d)/filt_init.o: $(d)/filt_param.o $(d)/cons_param.o $(d)/line_param.o
-$(d)/filt_param.o: $(d)/line_param.o $(d)/cons_param.o
-$(d)/forward.o: $(d)/line_param.o $(d)/cons_param.o $(d)/filt_param.o $(d)/inv_param.o $(d)/voigt.o $(d)/voigt_taylor.o $(d)/change_var.o
-$(d)/free_init.o: $(d)/inv_param.o $(d)/cons_param.o $(d)/filt_param.o
-$(d)/free_memory.o: $(d)/filt_param.o $(d)/line_param.o
-$(d)/inv_init.o: $(d)/cons_param.o $(d)/inv_param.o
-$(d)/inv_param.o: $(d)/cons_param.o
-$(d)/inv_utils.o: $(d)/cons_param.o $(d)/filt_param.o $(d)/inv_param.o $(d)/line_param.o $(d)/svdcmp.o $(d)/svbksb.o $(d)/ran_mod.o
-$(d)/lim_init.o: $(d)/inv_param.o
-$(d)/wfa_guess.o: $(d)/cons_param.o $(d)/filt_param.o $(d)/inv_param.o
-$(d)/ran_mod.o: $(d)/cons_param.o
-$(d)/invert.o: $(d)/invert.f90 $(d)/forward.o $(d)/line_param.o $(d)/cons_param.o $(d)/filt_param.o $(d)/inv_utils.o $(d)/inv_param.o $(d)/svdcmp.o $(d)/svbksb.o $(d)/wfa_guess.o $(d)/change_var.o
-$(d)/line_init.o: $(d)/line_param.o $(d)/cons_param.o
-$(d)/line_param.o: $(d)/cons_param.o
-$(d)/svbksb.o: $(d)/cons_param.o
-$(d)/svdcmp.o: $(d)/cons_param.o
-$(d)/voigt_data.o: $(d)/cons_param.o
-$(d)/voigt_init.o: $(d)/voigt_data.o $(d)/cons_param.o
-$(d)/voigt_taylor.o: $(d)/voigt_data.o $(d)/voigt_init.o $(d)/voigt.o $(d)/cons_param.o $(d)/line_param.o
-$(d)/wave_init.o: $(d)/line_param.o $(d)/cons_param.o
 
 $(VFISV_$(d)):		$(VFISV_FOBJ_$(d))
 
