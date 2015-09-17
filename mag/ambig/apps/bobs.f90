890f92229bceb0ab3bc1ca53a63b10b74127e036
cvs-fast-export <cvs-fast-export>
2015-09-17 20:25:36 +0000

    Synthetic commit for incomplete tag NetDRMS_Ver_8-10

--- mag/ambig/apps/bobs.f90
+++
@@ -1,6 +0,0 @@
-!***********************************************************************
-! Module containing the transverse component of the field.             *
-!***********************************************************************
-module bobs
-   real,dimension(:,:),allocatable :: bt
-end module bobs
