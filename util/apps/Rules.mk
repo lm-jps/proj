adfdb2cd920e93dea3222bf14277da35076a6f0f
phil <phil>
2015-10-18 22:47:13 +0000

    Initial commit of jsoc_getimages - gets AIA jp2000 times and URLs from normal query

--- util/apps/Rules.mk
+++ util/apps/Rules.mk
@@ -4,7 +4,8 @@ dirstack_$(sp)	:= $(d)
 d		:= $(dir)
 
 # Local variables
-MODEXE_$(d)	:= $(addprefix $(d)/, arithtool rebin2 ingest_from_fits hg_patch render_image index_convert hmi_limbdark drms2hdir pfss_backup jsoc_resize jsoc_rebin im_patch  hmi_fixCROTA2 aia_fixCROTA2 export_as_maproj hmib2ptr)
+MODEXE_$(d)	:= $ (addprefix $(d)/, arithtool rebin2 ingest_from_fits hg_patch render_image index_convert hmi_limbdark drms2hdir pfss_backup jsoc_resize jsoc_rebin im_patch  hmi_fixCROTA2 aia_fixCROTA2 export_as_maproj hmib2ptr jsoc_getimages)
+>>>>>>> 1.24
 MODEXE		:= $(MODEXE) $(MODEXE_$(d)) 
 
 MODEXE_SOCK_$(d):= $(MODEXE_$(d):%=%_sock)
