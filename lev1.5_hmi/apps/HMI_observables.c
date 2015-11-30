371c8316075ccb8568a69bc69e042b726a168d6f
arta <arta>
2015-11-30 15:23:29 +0000

    Im having a bad morning.

--- lev1.5_hmi/apps/HMI_observables.c
+++ lev1.5_hmi/apps/HMI_observables.c
@@ -1104,7 +1104,7 @@ int heightformation(int FID, double OBSVR, float *CDELT1, float *RSUN, float *CR
 
 char *observables_version() // Returns CVS version of Observables
 {
-  return strdup("$Id: HMI_observables.c,v 1.49 2015/11/30 15:02:56 arta Exp $");
+  return strdup("$Id: HMI_observables.c,v 1.50 2015/11/30 15:23:29 arta Exp $");
 }
 
 /*---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
@@ -1879,11 +1879,11 @@ char Lev1pSegName[60][5]={"I0","Q0","U0","V0","I1","Q1","U1","V1","I2","Q2","U2"
     {	
       if(DataCadence == 45.0)
 	{						             
-	  strcpy(HMISeriesLev15a,"hmi_test.V2_45s");              
-	  strcpy(HMISeriesLev15b,"hmi_test.M2_45s" );              
-	  strcpy(HMISeriesLev15c,"hmi_test.Ld2_45s" );              
-	  strcpy(HMISeriesLev15d,"hmi_test.Lw2_45s" );              
-	  strcpy(HMISeriesLev15e,"hmi_test.Ic2_45s" );
+	  strcpy(HMISeriesLev15a,"hmi.V_45s");              
+	  strcpy(HMISeriesLev15b,"hmi.M_45s" );              
+	  strcpy(HMISeriesLev15c,"hmi.Ld_45s" );              
+	  strcpy(HMISeriesLev15d,"hmi.Lw_45s" );              
+	  strcpy(HMISeriesLev15e,"hmi.Ic_45s" );
 	}
       if(DataCadence == 22.5)
 	{
