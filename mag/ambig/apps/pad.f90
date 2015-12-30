2a73835f49f8838979b17d1e322eb8488e262731
cvs-fast-export <cvs-fast-export>
2015-12-30 00:18:25 +0000

    Synthetic commit for incomplete tag globalhs_version_10

--- mag/ambig/apps/pad.f90
+++
@@ -1,8 +0,0 @@
-!***********************************************************************
-! Module containing the field plus padding for the potential field     *
-! calculation.                                                         *
-!***********************************************************************
-module pad
-   integer :: ixmin,ixmax,iymin,iymax,npad
-   real,dimension(:,:),allocatable :: blpad
-end module pad
