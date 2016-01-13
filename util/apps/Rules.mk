d61f6611493c1c8e2b230659699124c276c2c301
arta <arta>
2016-01-13 22:19:08 +0000

    From JPS.

--- util/apps/Rules.mk
+++ util/apps/Rules.mk
@@ -6,7 +6,9 @@ d		:= $(dir)
 # Local variables
 MODEXE_$(d)	:= $(addprefix $(d)/, arithtool rebin2 ingest_from_fits hg_patch render_image index_convert hmi_limbdark drms2hdir pfss_backup jsoc_resize jsoc_rebin im_patch  hmi_fixCROTA2 aia_fixCROTA2 export_as_maproj hmib2ptr jsoc_getJP2images)
 
-MODEXE		:= $(MODEXE) $(MODEXE_$(d)) 
+NONDEFAULTEXE_$(d) := $(addprefix $(d)/, store_lmbackup_v2)
+
+MODEXE		:= $(MODEXE) $(MODEXE_$(d)) $(NONDEFAULTEXE_$(d))
 
 MODEXE_SOCK_$(d):= $(MODEXE_$(d):%=%_sock)
 MODEXE_SOCK	:= $(MODEXE_SOCK) $(MODEXE_SOCK_$(d)) 
@@ -17,19 +19,20 @@ CEXE		:= $(CEXE) $(CEXE_$(d))
 SRCOBJ_$(d)     := $(addprefix $(d)/src/, heliographic_coords.o)
 
 EXE_$(d)	:= $(MODEXE_$(d)) $(CEXE_$(d)) 
-OBJ_$(d)	:= $(EXE_$(d):%=%.o) 
+OBJ_$(d)	:= $(EXE_$(d):%=%.o) $(NONDEFAULTEXE_$(d):%=%.o)
 DEP_$(d)	:= $(OBJ_$(d):%=%.d)
 CLEAN		:= $(CLEAN) \
 		   $(OBJ_$(d)) \
 		   $(EXE_$(d)) \
 		   $(MODEXE_SOCK_$(d)) \
+		   $(NONDEFAULTEXE_$(d)) \
 		   $(DEP_$(d)) \
                    $(SRCOBJ_$(d))
 
 
 TGT_BIN	        := $(TGT_BIN) $(EXE_$(d)) $(MODEXE_SOCK_$(d)) 
 
-S_$(d)		:= $(notdir $(EXE_$(d)) $(MODEXE_SOCK_$(d))) 
+S_$(d)		:= $(notdir $(EXE_$(d)) $(MODEXE_SOCK_$(d)) $(NONDEFAULTEXE_$(d)))
 
 # Local rules
 $(OBJ_$(d)):	$(SRCDIR)/$(d)/Rules.mk
