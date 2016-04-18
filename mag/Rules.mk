b940595ef84585cd316df33cb7b289a12fc44ea8
arta <arta>
2016-04-18 17:30:44 +0000

    Remove no-longer-used vfisv.

--- mag/Rules.mk
+++ mag/Rules.mk
@@ -5,9 +5,7 @@ d               := $(dir)
 
 # Subdirectories. Directory-specific rules are optional here. The
 # order NOT matter.
-# mag team probably isn't going to use apps dir
-# dir     := $(d)/apps
-# -include                $(SRCDIR)/$(dir)/Rules.mk
+
 dir     := $(d)/libs
 -include                $(SRCDIR)/$(dir)/Rules.mk
 dir     := $(d)/pfss
