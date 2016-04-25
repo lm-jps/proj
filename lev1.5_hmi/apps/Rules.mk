2b22900c55a19f7f45e60ae625933972a951907a
phil <phil>
2016-04-25 20:59:30 +0000

    Add HMI_lookup

--- lev1.5_hmi/apps/Rules.mk
+++ lev1.5_hmi/apps/Rules.mk
@@ -6,7 +6,7 @@ d		:= $(dir)
 # Local variables
 # NOTE: Add the base of the module's filename below (next to mymod)
 DEFMAKE_$(d)	:= $(addprefix $(d)/, HMI_observables HMI_IQUV_averaging correction_velocities phasemaps_voigt ingest_dcon HMI_observables_dcon HMI_observables_dcon2 lev1_dcon HMI_IQUV_averaging_dcon)
-GSLEXE_$(d)	:= $(addprefix $(d)/, HMI_observables2 undistort_lev1) $(DEFMAKE_$(d))
+GSLEXE_$(d)	:= $(addprefix $(d)/, HMI_lookup HMI_observables2 undistort_lev1) $(DEFMAKE_$(d))
 
 # THERE IS AN HMI_IQUV_averaging2.c IN THE SOURCE DIRECTORY, BUT THERE IS NO RULE TO MAKE THIS FILE.
 
