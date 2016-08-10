71b56ec1aa09867bc6ad5785044486f3c2cfce6d
sam <sam>
2016-08-10 23:08:08 +0000

    current

--- qmaps/scripts/htmlimages.py
+++ qmaps/scripts/htmlimages.py
@@ -2,7 +2,7 @@
 
 import cgi
 import math
-from datetime import datetime
+from datetime import datetime as dt
 import sys
 import os
 import mpld3
@@ -29,9 +29,7 @@ JSON_RV_SUCCESS = 0
 JSON_RV_BADARGS = 1
 JSON_RV_CREATEIMGFAIL = 2
 
-#radii is hard coded for the test. need to have the radii inside the data
 slice = 0
-radii = [1.001,1.050,1.100,1.150,1.250,1.500,1.750,2.000,2.250,2.499]
 manifest = {}
 carrot = None
 
@@ -80,11 +78,12 @@ class Arguments(object):
         except AttributeError as exc:
             raise Exception('args', 'Unknown argument: ' + name + '.')
 
-
-
+time = None
+radii = []
 def getData(car,series):
     #getData() takes 4 different fits files from the disk at a given carrington rotaion and returns them
-    global carrot 
+    global carrot
+    global time
     c = drms.Client()
     link = series + "[" + str(car) + "]"
     k, s = c.get(link, key=drms.const.all, seg='synop,chmap,logq,brq')
@@ -94,6 +93,12 @@ def getData(car,series):
     chmap = fits.open(s.loc[0,"chmap"])
     logq = fits.open(s.loc[0,"logq"])
     brq = fits.open(s.loc[0,"brq"])
+    time = k.T_OBS.tolist()[0]
+    
+    for rad in range(1,11):
+        r = 'R' + format(rad, '02d')
+        radii.append(k.loc[0,r])
+        
     return logq, brq,chmap,synop
 
 def getImageData():
@@ -110,9 +115,9 @@ def getImageData():
 def createImages(wr,series):
     #the first part here takes the carrington rotation and tranforms it into the day month and year that the carrot began
     global carrot
-    JD = 27.2753*(carrot - 1.0) + 2398167.0
-    t = Time(JD,format = 'jd')
-    realDate = t.datetime.strftime('%B %d, %Y')
+    global radii
+    global fr
+    realDate = getDate()
     #creates a manifest that the javascript and html reference in order to find the images
     manifest['slogfilenames'] = {}
     manifest['carrot'] = carrot
@@ -125,38 +130,65 @@ def createImages(wr,series):
     colormap_customized = mcol.ListedColormap(normalcolors)
     
     for slice in range(len(slogq_imageData[:,0,0])):
-        plt.cla()
-        slogq_slice = slogq_imageData[slice,:,:]
-        absslogq_slice = np.abs(slogq_slice)
-        colormax = np.nanmax(absslogq_slice)
-        plt.imshow(slogq_slice, cmap = colormap_customized, origin = 'lower', extent = [0,360,-90,90], vmax = 3, vmin = -3)
-        plt.title('S-logQ Map at %.3f Solar Radii' % radii[slice], y = 1.09)
-        axisLines()
         SlogfileName = 'img/' + str(carrot) + 'slogqmap%.3f.html'  %radii[slice]
-        with open(os.path.join(wr, SlogfileName), 'w') as mpld3image:
-            mpld3.save_html(fig,mpld3image)   
-        manifest['slogfilenames']["%.3f" %radii[slice]] = SlogfileName
+
+        if fr == False:
+            
+            if os.path.exists(wr + SlogfileName):
+                manifest['slogfilenames']["%.3f" %radii[slice]] = SlogfileName
+            
+        else:
+            plt.cla()
+            slogq_slice = slogq_imageData[slice,:,:]
+            absslogq_slice = np.abs(slogq_slice)
+            colormax = np.nanmax(absslogq_slice)
+            plt.imshow(slogq_slice, cmap = colormap_customized, origin = 'lower', extent = [0,360,-90,90], vmax = 3, vmin = -3)
+            plt.title('S-logQ Map at %.3f Solar Radii' % radii[slice], y = 1.09)
+            
+            axisLines()
+            
+            with open(os.path.join(wr, SlogfileName), 'w') as mpld3image:
+                mpld3.save_html(fig,mpld3image)   
+            manifest['slogfilenames']["%.3f" %radii[slice]] = SlogfileName
         
     #saves the coronal hole map and the synoptic map to the same directory as the slogqmaps 
-    plt.cla()
-    chmap_slice = np.asarray(chmapData[1].data, dtype=np.float)
-    plt.imshow(chmap_slice, origin = 'lower', extent = [0,360,-90,90], cmap = plt.cm.get_cmap('seismic_r'))
-    plt.title('Coronal Hole Map', y = 1.09)
-    axisLines()
     ChmapfileName = 'img/' + str(carrot) + 'chmap.html'
-    with open(os.path.join(wr, ChmapfileName), 'w') as mpld3image:
-        mpld3.save_html(fig,mpld3image)
-    manifest['chfilename'] = ChmapfileName
+
+    if fr == False:
         
-    plt.cla()
-    synop_slice = synopData[0].data
-    plt.imshow(synop_slice, origin = 'lower', extent = [0,360,-90,90], cmap = plt.cm.get_cmap('Greys'))
-    plt.title('Synoptic Map', y = 1.09)
-    axisLines()
+        if os.path.exists(wr + ChmapfileName):
+            manifest['chfilename'] = ChmapfileName
+        
+    else:
+        plt.cla()
+        chmap_slice = np.asarray(chmapData[1].data, dtype=np.float)
+        plt.imshow(chmap_slice, origin = 'lower', extent = [0,360,-90,90], cmap = plt.cm.get_cmap('seismic_r'))
+        plt.title('Coronal Hole Map', y = 1.09)
+        
+        axisLines()
+        
+        with open(os.path.join(wr, ChmapfileName), 'w') as mpld3image:
+            mpld3.save_html(fig,mpld3image)
+        manifest['chfilename'] = ChmapfileName
+    
     SynopfileName = 'img/' + str(carrot) + 'synop.html'
-    with open(os.path.join(wr, SynopfileName), 'w') as mpld3image:
-        mpld3.save_html(fig,mpld3image)        
-    manifest['synopfilename'] = SynopfileName
+    
+    if fr == False:
+        
+        if os.path.exists(wr + SynopfileName):
+            manifest['synopfilename'] = SynopfileName
+        
+    else:
+        plt.cla()
+        synop_slice = synopData[0].data
+        plt.imshow(synop_slice, origin = 'lower', extent = [0,360,-90,90], cmap = plt.cm.get_cmap('Greys'))
+        plt.title('Synoptic Map', y = 1.09)
+        
+        axisLines()
+        
+        with open(os.path.join(wr, SynopfileName), 'w') as mpld3image:
+            mpld3.save_html(fig,mpld3image)        
+        manifest['synopfilename'] = SynopfileName
     
     #prints the manifest into a text file that is json serializable    
     jsonString = json.dumps(manifest)
@@ -172,6 +204,16 @@ def axisLines():
     plt.vlines(0,-90,90, linewidth = 1)
     ax.set_xlabel("Degrees Longitude(°)", y = 1.2)
     ax.set_ylabel("Degrees Latitude(°)", x = 1.2)
+
+def getDate():
+    
+    global time
+
+    newTime = time.replace("_TAI","")
+    d = dt.strptime(newTime,"%Y.%m.%d_%H:%M:%S_%Z")
+    realTime = d.strftime("%H:%M:%S %B %d, %Y %Z")
+
+    return realTime
     
 if __name__ == "__main__":
     #runs all of the functions
@@ -196,11 +238,13 @@ if __name__ == "__main__":
                         arguments.setArg('carrot', int(val))
                     elif key in ('webroot'):
                         arguments.setArg('webroot', val)
+                    elif key in ('force'):
+                        arguments.setArg('force', int(val))
                     else:
                         raise ValueError
         except ValueError:
             jsonRoot['status'] = JSON_RV_BADARGS
-            jsonRoot['errMsg'] = 'Invalid CGI URL.\nUsage:\n  http://jsoc.stanford.edu/cgi-bin/qmapviewer?series=<DRMS qmap series>&carrot=<Carrington Rotation>&webroot=<web root directory>'
+            jsonRoot['errmsg'] = 'Invalid CGI URL.\nUsage:\n  http://jsoc.stanford.edu/cgi-bin/qmapviewer?series=<DRMS qmap series>&carrot=<Carrington Rotation>&webroot=<web root directory>&force=<true/false>'
             error = True
 
     else:    
@@ -208,6 +252,7 @@ if __name__ == "__main__":
         parser.add_argument('series', '--series', help='The DRMS data series that contains the input qmap images.', metavar='<qmap series>', dest='series', required=True)
         parser.add_argument('carrot', '--carrot', help='The Carrington Rotation for the qmap image.', metavar='<Carrington Rotation>', dest='carrot', type=int, required=True)
         parser.add_argument('webroot', '--webroot', help='The directory in which the images will be placed.', metavar = '<Web Root>', dest='webroot', required=True)
+        parser.add_argument('force', '--force', help="If 'True' then the program will be forced to recreate the set of images that the user has chosen.", metavar =  '<force>', dest='force', type=int)
         arguments = Arguments(parser)
 
     if not error:
@@ -215,7 +260,11 @@ if __name__ == "__main__":
             wr = arguments.webroot
             car = arguments.carrot
             ds = arguments.series
-
+            if hasattr(arguments, "force") and arguments.force == 1:
+                fr = True
+            else:
+                fr = False
+                
             logqData, brqData, chmapData, synopData = getData(car,ds)
             slogq_imageData = getImageData()
             np.nanmax(slogq_imageData)
@@ -227,6 +276,13 @@ if __name__ == "__main__":
         except:
             if webRun:
                 jsonRoot['status'] = JSON_RV_CREATEIMGFAIL
+                rv = sys.exc_info()
+                if rv[0] and rv[1]:
+                    jsonRoot['errmsg'] = str(rv[0]) + " error: " + str(rv[1])
+                else:
+                    jsonRoot['errmsg'] = "Internal error"
+            else:
+                raise
 
     if webRun:
         print('Content-type: application/json\n')
