571b1cf0fafb1a574a083f3eeeb975b290232efb
cvs-fast-export <cvs-fast-export>
2017-03-20 21:01:58 +0000

    Synthetic commit for incomplete tag NetDRMS_Ver_9-0

--- mag/harp/libs/matlab/mfile-mex/fits/Local.mk
+++
@@ -1,8 +0,0 @@
-#
-# ...customized to use fits libraries!
-#
-
-# customize it by adding FITSIO_LIBS to the libraries given to mex
-# note, FITSIO_LIBS is defined in the MatlabMakeRules
-LIBSUSED += $(FITSIO_LIBS)
-
