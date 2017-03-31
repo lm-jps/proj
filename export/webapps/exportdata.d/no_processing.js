08edb62cd09757ece486acaec74dd1666e2c7646
cvs-fast-export <cvs-fast-export>
2017-03-31 20:02:28 +0000

    Synthetic commit for incomplete tag globalhs_version_17

--- export/webapps/exportdata.d/no_processing.js
+++
@@ -1,9 +0,0 @@
-//
-// This file contains a list of series for which the present export processing will fail
-//
-var NoProcessingList = [
-  "hmi.sharp_720s",
-  "hmi.sharp_720s_nrt",
-  "hmi.cea_720s",
-  "hmi.cea_720s_nrt",
-  "ENDOFLIST"];
--- lev1.5_hmi/libs/lev15/fstats.h
+++
@@ -1,4 +0,0 @@
-static double median(int n, float arr[]);
-
-int fstats(int n, float arr[], double *min, double *max, double *medn,
-	   double *mean, double *sig, double *skew, double *kurt, int *ngood);
