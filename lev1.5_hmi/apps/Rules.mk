d9d5ba42d72d517260d44173e2643813b781cf21
arta <arta>
2017-11-02 22:32:30 +0000

    fix build

--- lev1.5_hmi/apps/Rules.mk
+++ lev1.5_hmi/apps/Rules.mk
@@ -5,7 +5,7 @@ d		:= $(dir)
 
 # Local variables
 # NOTE: Add the base of the module's filename below (next to mymod)
-DEFMAKE_$(d)	:= $(addprefix $(d)/, HMI_observables_90q HMI_observables HMI_IQUV_averaging_YL HMI_IQUV_averaging correction_velocities ingest_dcon ingest_dcon_gen HMI_observables_dcon HMI_observables_dcon2 lev1_dcon HMI_IQUV_averaging_dcon HMI_observables_135q)
+DEFMAKE_$(d)	:= $(addprefix $(d)/, HMI_observables_90q HMI_observables HMI_IQUV_averaging correction_velocities ingest_dcon ingest_dcon_gen HMI_observables_dcon HMI_observables_dcon2 HMI_observables_dconS lev1_dcon HMI_IQUV_averaging_dcon HMI_observables_135q)
 GSLEXE_$(d)	:= $(addprefix $(d)/, HMI_lookup HMI_observables2 undistort_lev1 phasemaps_voigt) $(DEFMAKE_$(d))
 
 # THERE IS AN HMI_IQUV_averaging2.c IN THE SOURCE DIRECTORY, BUT THERE IS NO RULE TO MAKE THIS FILE.
