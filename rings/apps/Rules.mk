1f7cfdf6166e1c36ef0b3c86ccec3a35e1f088a3
arta <arta>
2018-06-07 17:01:23 +0000

    add rdfitf to the standard make system

--- rings/apps/Rules.mk
+++ rings/apps/Rules.mk
@@ -4,11 +4,8 @@ dirstack_$(sp)	:= $(d)
 d		:= $(dir)
 
 # Local variables
-# NOTE: Add the base of the module's filename below (next to mymod)
-RDVINV_$(d)	:= 
-ifeq ($(JSOC_MACHINE), linux_x86_64)
 RDVINV_$(d)	:= $(addprefix $(d)/, rdvinv)
-endif
+RDFITF_$(d)	:= $(addprefix $(d)/, rdfitf)
 
 ifeq ($(COMPILER), icc)
   MODEXE_$(d)	:= $(addprefix $(d)/, datavg maicalc maproj mtrack pspec3 rdcover rdfitc xtrackd) 
@@ -18,15 +15,17 @@ MODEXE		:= $(MODEXE) $(MODEXE_$(d))
 MODEXE_SOCK_$(d):= $(MODEXE_$(d):%=%_sock)
 MODEXE_SOCK	:= $(MODEXE_SOCK) $(MODEXE_SOCK_$(d))
 
-MODEXE_USEF_$(d):= $(RDVINV_$(d))
+MODEXE_USEF_$(d):= $(RDVINV_$(d)) $(RDFITF_$(d))
 MODEXE_USEF	:= $(MODEXE_USEF) $(MODEXE_USEF_$(d))
 
 CEXE_$(d)	:= $(addprefix $(d)/, gentargs) 
 CEXE		:= $(CEXE) $(CEXE_$(d))
 
-EXE_$(d)        := $(MODEXE_$(d)) $(MODEXE_USEF_$(d) $(CEXE_$(d))
-OBJ_OLAXY_$(d)	:= $(addprefix $(d)/, ola_xy_v12.o ola_subs.o)
-OBJ_$(d)	:= $(EXE_$(d):%=%.o) $(OBJ_OLAXY_$(d))
+EXE_$(d)        := $(MODEXE_$(d)) $(MODEXE_USEF_$(d)) $(CEXE_$(d))
+OBJ_OLAXY_$(d)	:= $(addprefix $(d)/, ola_xy_v13.o ola_subs.o)
+OBJ_F_$(d)      := $(addprefix $(d)/, cart_to_polar.o fourier_filter.o ring_pass.o polyval.o multfactor.o func1.o first_deriv_1.o second_deriv.o dogleg3.o hmifits.o ringanalysis.o ccint2.o fourierlibn32.o)
+
+OBJ_$(d)	:= $(EXE_$(d):%=%.o) $(OBJ_OLAXY_$(d)) $(OBJ_F_$(d))
 DEP_$(d)	:= $(OBJ_$(d):%=%.d)
 CLEAN		:= $(CLEAN) \
 		   $(OBJ_$(d)) \
@@ -44,10 +43,9 @@ $(OBJ_$(d)):		CF_TGT := $(CF_TGT) $(FFTWH)
 
 $(MODEXE_$(d)) $(MODEXE_SOCK_$(d)):	LL_TGT := $(LL_TGT) $(FFTW3LIBS) $(FFTW3FLIBS) 
 
-ifeq ($(JSOC_MACHINE), linux_x86_64)
 $(RDVINV_$(d)):				$(OBJ_OLAXY_$(d))
-$(RDVINV_$(d)):				LL_TGT := $(LL_TGT) -lmkl_em64t
-endif
+$(RDFITF_$(d)):				$(OBJ_F_$(d))
+$(RDVINV_$(d)) $(RDFITF_$(d)):				LL_TGT := $(LL_TGT) -lmkl_em64t
 
 # Shortcuts
 .PHONY:	$(S_$(d))
