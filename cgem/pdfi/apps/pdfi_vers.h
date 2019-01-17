b0d761ec89796cab079fb776125528c0baca4d59
xudong <xudong>
2019-01-17 03:55:16 +0000

    fixed a typo

--- cgem/pdfi/apps/pdfi_vers.h
+++ cgem/pdfi/apps/pdfi_vers.h
@@ -4,4 +4,4 @@
 // const char *pdfi_vers = "1f82c5e750";            // Jul 25 2018 XS
 // const char *pdfi_vers = "4344acd102";            // Aug 16 2018 XS
 //const char *pdfi_vers = "b9bac98652";               // Nov 07 2018 XS
-const char *pdfi_vers = "11e3eaee40";               // Nov 07 2018 XS
+const char *pdfi_vers = "11e3eaee40";               // Jan 16 2019 XS
