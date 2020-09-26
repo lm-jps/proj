fc010f0dea8ee77770d5a8ff2788c0cf15e47f46
xudong <xudong>
2020-09-26 05:11:16 +0000

    Final version of localpf.c using Green's function method; changed to compressed output; implemented Kahan summation; zoff in Green's function changed from 0.5 to 1./sqrt(2*M_PI) according to Sakurai 1982 eq 2.14

--- mag/nlfff/apps/green.c
+++ mag/nlfff/apps/green.c
@@ -6,6 +6,7 @@
  * Adapted by X. Sun
  * Version 1.0 Feb 18 2010
  * Version 1.1 Mar 16 2010
+ * Changed zoff Sep 25 2020
  *    Moved from relax.c
  */
 
@@ -21,37 +22,32 @@ void green(double *bz0,
            int nx, int ny, int nz, 
            int verb)
 {
-    double *x, *y, *z, *Pot0;	// ---- Redefined *x?
+    double *x, *y, *z, *Pot0;
     double zoff, dummy1, h, r, rx, ry, rz;
-  //int nx, ny, nz;	// ---- PASSED IN
-    int nynz, nxnynz;	// ---- Redefined?
+    int nynz, nxnynz;
     int i, i2, ix, iy, iz, ix1, iy1;
 
-    if (verb) {printf("Vectormagnetogram loaded\n"); fflush(stdout);}
+    if (verb) {printf("Vector magnetogram loaded\n"); fflush(stdout);}
 
     h = 1.0;
     nynz = ny * nz;
     nxnynz = nx * ny * nz;
-    zoff = 0.5;
-{printf("1\n"); fflush(stdout);}
-    // ????
+    zoff = 1.0 / sqrt(2 * M_PI);        // Sakurai (1982) (Eq 2.14)
+
     x = (double *) calloc(nx, sizeof(double));
     y = (double *) calloc(ny, sizeof(double));
     z = (double *) calloc(nz, sizeof(double));
     Pot0 = (double *) calloc(nxnynz, sizeof(double));
-    {printf("2\n"); fflush(stdout);}
-
-    // NEED TO CHECK ORDER!!!!!!!!
-  //bz0 = (double *) calloc(nx*ny, sizeof(double));	// ---- PASSED IN
 
     for (ix = 0; ix < nx; ix++) {x[ix] = ix * 1.0;}
     for (iy = 0; iy < ny; iy++) {y[iy] = iy * 1.0;}
     for (iz = 0; iz < nz; iz++) {z[iz] = zoff + iz * 1.0;}
-{printf("3\n"); fflush(stdout);}
+    
     /* Calculate Potential */
 
+    double c, yt, t;    // kahan summation
 #ifdef _OPENMP
-#pragma omp parallel for private (i,ix,iy,iz,dummy1,ix1,iy1,rx,ry,rz,r,i2)
+#pragma omp parallel for private (i,ix,iy,iz,dummy1,ix1,iy1,rx,ry,rz,r,i2,c,yt,t)
 #endif
     for (ix = 0; ix < nx; ix++) {
         if (verb) {printf("percent finished = %lf\n", 100.0 * ix / nx); fflush(stdout);}
@@ -59,6 +55,7 @@ void green(double *bz0,
         for (iz = 0; iz < nz; iz++) {
             i = ix * nynz + iy*nz + iz;
             dummy1 = 0.0;
+            c = 0.0;
             for (ix1 = 0; ix1 < nx; ix1++)
             for (iy1 = 0; iy1 < ny; iy1++) {
                 i2 = ny * ix1 + iy1;
@@ -66,12 +63,15 @@ void green(double *bz0,
                 ry = y[iy] - y[iy1];
                 rz = z[iz];
                 r = sqrt(rx * rx + ry * ry + rz * rz);
-                dummy1 = dummy1 - bz0[i2] / r;
+                yt = - bz0[i2] / r - c;
+                t = dummy1 + yt;
+                c = (t - dummy1) - yt;
+                dummy1 = t;
             }	// ix1, iy1
             Pot0[i] = dummy1 / 2.0 / M_PI;
         }	// iy, iz
     }	// ix
-{printf("4\n"); fflush(stdout);}
+    
     /* B_pot */
 
 #ifdef _OPENMP
@@ -86,14 +86,9 @@ void green(double *bz0,
         By[i] = GRADY(Pot0,i);
         Bz[i] = GRADZ(Pot0,i);
     }
-/*
-printf("\n********************************************\n");
-printf("Bzpf[1]=%f, Bzpf[2]=%f\n", Bz[1], Bz[2]);
-printf("********************************************\n");
-*/
-{printf("5\n"); fflush(stdout);}
+    
     /* Clean up */
     free(x); free(y); free(z); free(Pot0);
-{printf("6\n"); fflush(stdout);}
+    
     if (verb) printf("Potential field done\n");
 }
