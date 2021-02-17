162dd2b40cff3ccffc23b741d02081e76695fe18
arta <arta>
2021-02-17 18:56:40 +0000

    remove from x86_64 build

--- mag/nlfff/Rules.mk
+++ mag/nlfff/Rules.mk
@@ -3,12 +3,13 @@ sp              := $(sp).x
 dirstack_$(sp)  := $(d)
 d               := $(dir)
 
+ifeq ($(JSOC_MACHINE), linux_avx)
 # Subdirectories. Directory-specific rules are optional here. The
 # order NOT matter.
 dir     := $(d)/apps
 -include                $(SRCDIR)/$(dir)/Rules.mk
+endif
 
 # Standard things
 d               := $(dirstack_$(sp))
 sp              := $(basename $(sp))
-
