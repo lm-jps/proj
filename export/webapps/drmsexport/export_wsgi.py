c60fd9498631b61b0987b8b95694779c4e1fcd63
arta <arta>
2021-09-22 16:15:58 +0000

    Changes code to obtain drms parameters from the installed drms_parameters package

--- export/webapps/drmsexport/export_wsgi.py
+++ export/webapps/drmsexport/export_wsgi.py
@@ -2,14 +2,13 @@
 #   uwsgi --socket 171.64.103.154:50000 --protocol=http -w export_wsgi:export
 # ultimately, we will create and provide to uwsgi an .ini file that contains the desired parameters
 
-from sys import path as sp
-from os import path as op
-
-sp.append(op.join(op.dirname(op.realpath(__file__)), '../../../include'))
-from drmsparams import DRMSParams
+from drms_parameters import DRMSParams
 
 # import the flask app from export.py
 from export import export
 
 if __name__ == "__main__":
+    drms_params = DRMSParams()
     export.run(host=drms_params.EXPORT_WEB_SERVER, port=drms_parms.EXPORT_WEB_SERVER_PORT)
+else:
+    pass
